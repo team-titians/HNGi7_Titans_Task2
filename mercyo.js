6386408c0976b831de9547778121e66ab998aae1
Mercy Okobi <mercyokobi@gmail.com>
2020-06-03 13:55:33 +0100

    Mercyo  Stage 2 Commit

---
+++ mercyo.js
@@ -0,0 +1,6 @@
+function taskExercise(){
+    var greeting = "Hello World, this is Mercy Okobi with HNGi7 ID HNG-01060 and email mercyokobi@gmail.com using JavaScript for stage 2 task"
+    return greeting;
+} 
+
+console.log(taskExercise());
