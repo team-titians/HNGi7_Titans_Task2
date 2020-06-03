7ed3bdae02bcaaff4150380c52fedc123cf0258f
Michael <lordkay07@gmail.com>
2020-06-03 15:16:19 +0100

    MiCkY-HNG-02942 script

---
+++ scripts/MiCkY-HNG-02942.js
@@ -0,0 +1,6 @@
+function task2 () {
+    return ("Hello World, this is Michael David with HNGi7 ID HNG-02942 and email lordkay07@gmail.com using JavaScript for stage 2 task");
+}
+
+
+console.log(task2());
