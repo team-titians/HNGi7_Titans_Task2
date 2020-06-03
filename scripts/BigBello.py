1704d7ebe8e04d2cfefe8434c8229b1e9c3fae70
bauxitex3 <66308671+bauxitex3@users.noreply.github.com>
2020-06-03 14:54:13 -0400

    Update BigBello.py

--- scripts/BigBello.py
+++ scripts/BigBello.py
@@ -1,12 +1,13 @@
 def main(): 
-   intro = "Hello World"
+   intro = "Hello World!"
    full_name= "Malcom Biggy Bello"
    HNGi7_ID="HNGi7 ID: HNG-02832"
-   ID= "BigBello"
-   outro="using  PYTHON for stage 2 task"
-   full_message = intro + "\n" + full_name + "\n" + HNGi7_ID + "\n" + ID + "\n" + outro
+   email='bauite.x3@gmail.com'
+   ID= "ID: BigBello"
+   outro="using  PYTHON3 for stage 2 task"
+   full_message = intro+' this is with '+"\n"+full_name+"\n"+HNGi7_ID+ "\n"+email+"\n"+ID+"\n"+outro
    print(full_message)
    #return full_message
-   
+ 
 if __name__ == "__main__":
    main() 
