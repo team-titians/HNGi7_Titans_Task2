4d73f925f721698b2d457a85ce82a8c3c954bcaf
0y3 <trivin98@gmail.com>
2020-06-03 23:49:36 +0100

    update code

--- scripts/0y3.php
+++ scripts/0y3.php
@@ -1,6 +1,6 @@
 <?php
 $NAME = "OYEWOLE SEGUN";
 $INTERNSHIPID = "HNG-05488";
-$EMAIL= "trivin98@gmail.com";
-$lang = "PHP";
-echo("'Hello World, this is '.$NAME.' with HNGi7 ID '.$INTERNSHIPID.' using '.$lang.' for stage 2 task';");
+$email= "trivin98@gmail.com";
+$language = "PHP";
+echo("'Hello World, this is '.$NAME.' with HNGi7 ID '.$INTERNSHIPID.' and email '.$email.' using '.$language.' for stage 2 task';");
