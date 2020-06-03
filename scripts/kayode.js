6c6e60301c1710d2feb75726166b3c664f01a269
lollykrown <joe_kayu@yahoo.com>
2020-06-03 17:38:15 +0100

    commit for kayode agboola
    commit for kayode agboola

---
+++ scripts/kayode.js
@@ -0,0 +1,5 @@
+const name = 'Kayode Agboola';
+const email = 'joe_kayu@yahoo.com';
+const hngId = 'HNG-00151';
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hngId} and email ${email} using nodeJs for stage 2 task`);
