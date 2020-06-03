5b21bf11bc4b19eb49027ee7dd89af10fe75dabe
bauxitex3 <66308671+bauxitex3@users.noreply.github.com>
2020-06-03 15:07:31 -0700

    Update BigBello.py

--- scripts/BigBello.py
+++ scripts/BigBello.py
@@ -1,12 +1,12 @@
 def main():  
-   full_name= "Malcom Biggy Bello"
-   HNGi7_ID="HNG-02832"
+   name= "Malcom Biggy Bello"
+   ID="HNG-02832"
    email='bauite.x3@gmail.com'  
    using = "Python3"  
    output=print(
          "Hello World!, "+
-         'this is '+ full_name+
-         " with HNGi7 ID "+HNGi7_ID+ 
+         'this is '+ name+
+         " with HNGi7 ID "+ID+ 
          " and email " + email+
          ' using '+using+ 
          " for stage 2 task"
