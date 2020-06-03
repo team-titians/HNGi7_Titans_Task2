d0a15b9f78e624a83ef7f8c048ffa934ac5f6afd
Peteradeleke <Peteradeleke04@gmail.com>
2020-06-03 23:18:09 +0100

    added my script file

---
+++ scripts/Peteru.js
@@ -0,0 +1,9 @@
+let myName = "Adeleke Peter Ayooluwa";
+let myHNGi7Id = "HNG - 03826";
+let myEmail = "Peteradeleke04@gmail.com";
+
+function hello() {
+    console.log(`Hello World, this is ${myName} with HNGi7 ID ${myHNGi7Id} and email ${myEmail} using Javascript for stage 2 task.`);
+}
+
+hello();
