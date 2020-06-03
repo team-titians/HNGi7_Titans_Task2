1094e04e8c530ccbfa6a9cf4f2385763e06da2d1
timothymayor <timothymayor24@gmail.com>
2020-06-03 20:50:16 +0100

    deleted unnecessary files

--- Arky.js
+++
@@ -1,4 +0,0 @@
-let a = "Hello World, this is Abdulhaqq Motolani Somoye with HNGi7 ID HNG-02662";
-let b = " and email motolanisomoye@gmail.com using javascript for stage 2 task";
-    
-    console.log(a +" "+b);
