7b9c66c0807622ee20fefdb1e96e1b937ccf5af3
kyle2f <bello4aus@gmail.com>
2020-06-03 21:39:45 +0100

    update hng

--- scripts/hng_intern_tomi.py
+++ scripts/hng_intern_tomi.py
@@ -1,13 +1,7 @@
 name = "Bello Oluwatomisin"
-id = "HNG-04743"
+hngi7_id = "HNG-04743"
 language = "Python"
 email = "bello4aus@gmail.com"
 
-def main():
-    name = "Bello Oluwatomisin"
-    ID = "tomii"
-    language = "Python"
-    print(f"Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task.")
 
-if __name__ == "__main__":
-    main()
+print(f"Hello World, this is {name} with HNGi7 ID {hngi7_id} and email {email} using {language} for stage 2 task.")
