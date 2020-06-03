0d844c5ac6c28443da801843601ecfdce45b8284
Charles Kendrick <charlesrichson@gmail.com>
2020-06-03 12:18:22 +0100

    Script implemented

--- scripts/charles_duruaku.js
+++ scripts/charles_duruaku.js
@@ -1,4 +1,12 @@
-const data =
-  "Hello World, this is Charles Duruaku with HNGi7 ID HNG-05100 and email charlesrichson@gmail.com using JavaScript for stage 2 task";
+// Script to return data based on this format: Hello World, this is [Your full name] with HNGi7 ID [Your HNG ID] and [email] using [language Used] for stage 2 task
 
-console.log(data);
+const data = {
+  name: "Duruaku Charles",
+  id: "HNG-05100",
+  language: "JavaScript",
+  email: "charlesrichson@gmail.com"
+};
+
+const returnedData = `Hello World, this is ${data.name} with HNGi7 ID ${data.id} and email ${data.email} using ${data.language} for stage 2 task`;
+
+console.log(returnedData);
