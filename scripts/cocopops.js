892cbd6c4a2d78510fe116e02073f95011e5cd8a
nehkee <46467065+nehkee@users.noreply.github.com>
2020-06-03 21:50:59 +0100

    add cocoposp

---
+++ scripts/cocopops.js
@@ -0,0 +1,7 @@
+
+ function sayHello(){
+   const name = 'Hello world, ';
+
+  console.log(name + 'this is Chukwuneke Prince  with HNGi7 ID 00066 and my email address is chukwunekeprince@gmail.com using node for task two');
+}
+sayHello();
