a3374f1c65d17e4a04183728aed703af83f9a129
Trillzdboy <61975856+Trillzdboy@users.noreply.github.com>
2020-06-03 23:26:40 +0100

    Add files via upload

---
+++ trillzdboy.js
@@ -0,0 +1,5 @@
+var name= "Agulue Valentine";
+const HNGid= "HNG-01383";
+const language="Javascript";
+const email= "chukwudubem7@gmail.com"
+console.log ( "Hello World, this is " +name+ " with HNGi7 ID " + HNGid+ " and email " +email+  " using " +language+  " for stage 2 task" )
