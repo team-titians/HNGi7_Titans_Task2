8850a0b1757e02eb852774e9f9eeb10da4d65d25
Bukunmi Kolawole <61963617+Bkm-hngi7@users.noreply.github.com>
2020-06-04 00:32:40 +0100

    Update bkm.py

--- scripts/bkm.py
+++ scripts/bkm.py
@@ -1,5 +1,5 @@
-hng_id = ("HNG-00647")
-language = ("Python")
-email = ("bukunmikolawole01@gmail.com")
-code = (f"this is Bukunmi Kolawole with HNGi7 ID {hng_id} using {language} for stage 2 task.")
-print (f"Hello World {code} with email: {email}")
+name = "this is Bukunmi Kolawole"
+hngID = 'HNG-00647'
+language = "Python"
+email = "bukunmikolawole01@gmail.com"
+print (f"Hello World, {name} with HNGi7 ID {hngID} and email {email} using {language} for stage 2 task.")
