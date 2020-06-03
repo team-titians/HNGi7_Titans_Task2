68ec990fc33cf200a690c23e74bf64229673042c
OlaiyaA <66248649+OlaiyaA@users.noreply.github.com>
2020-06-03 21:47:02 +0100

    Update Olaiya.js
    making some changes

--- scripts/Olaiya.js
+++ scripts/Olaiya.js
@@ -1,3 +1,1 @@
-const Name = 'Olaiya Abdulmalik'   ,  ID = 'HNG-05377', Email = 'olaiya.abdulmalik@gmail.com', language = 'JavaScript';
-
-console.log('Hello World, this is ' + Name + ' with HNGi7 ID ' + ID + ' and e-mail ' + Email + ' using ' + language + ' for stage 2 task');
+console.log("Hello World, this is Olaiya Abdulmalik with HNGi7 ID HNG-05377 and email olaiya.abdulmalik@gmail.com using javascript for stage 2 task");
