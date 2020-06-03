daf24164876317cdfe228b6c573a093ef489c693
robertechie <62170917+robertechie@users.noreply.github.com>
2020-06-03 16:04:27 +0100

    ss

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ foreach($scripts as $script){
              exec("python3 ".$script_location, $output, $return_val);
         break; 
         case 'js':
-            exec("node ".$script_location .' 2>&1',  $output, $return_val);
+            exec("node ".$script_location .' 2>&12',  $output, $return_val);
         case 'php':
             exec("php ".$script_location,  $output, $return_val);
         break;
