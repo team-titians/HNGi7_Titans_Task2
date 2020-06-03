c85368fe748fe6879e27e7b384cff6189f5ce523
ab3id <abeidtwo@gmail.com>
2020-06-03 22:12:08 +0300

    fixed failed status on Ab3id.php script

--- scripts/ab3id.php
+++ scripts/ab3id.php
@@ -3,5 +3,5 @@ $fullName = "Abeid Juma";
 $emailAddress = "abeidtwo@gmail.com";
 $hngID = "HNG-01319";
 $language = "PHP";
-$htmlOutput = "Hello World, this is $fullName with HNGi7 ID $hngID and Email $emailAddress using $language for stage 2 task";
+$htmlOutput = "Hello World, this is $fullName with HNGi7 ID $hngID and email $emailAddress using $language for stage 2 task";
 echo $htmlOutput;
