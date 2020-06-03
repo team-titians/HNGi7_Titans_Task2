b988603d2126882065b34c7abd8c0cd3a9d2af3e
Ochuba <38507501+Ochuba@users.noreply.github.com>
2020-06-03 18:00:23 +0100

    Update ochuba_Samuel.js

--- scripts/ochuba_Samuel.js
+++ scripts/ochuba_Samuel.js
@@ -1,8 +1,5 @@
 // Script to output my details.
 
-let details = (name, HNG_id, email, stack) =>{
 
-    return `Hello World, this is ${name} with HNGi7 ID ${HNG_id} and email ${email} using ${stack} for stage 2 task`
-};
+console.log("Hello World, this is Kevin Uti with HNGi7 ID HNG-04297 and email kvuti10@gmail.com using javascript for stage 2 task");
 
-console.log(details('Ochuba Samuel', 'HNG-04010', 'esambooks@gmail.com', 'Javascript'));
