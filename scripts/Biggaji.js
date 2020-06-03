30c1cf8b11993dcafa398772761455803f382958
Tobi Ajibade <40983352+HerityJohnny@users.noreply.github.com>
2020-06-03 14:11:41 -0700

    initial new file

---
+++ scripts/Biggaji.js
@@ -0,0 +1,7 @@
+var fullName = 'Tobiloba Ajibade';
+var hngid = 'HNG-03525';
+var lang = 'nodejs';
+var email = 'herityjohnny14@gmail.com';
+
+//The output 
+console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + hngid + ' and email ' + email + ' using ' + lang + ' for stage 2 task');
