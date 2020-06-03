b565d4e4edcfb5b5fdd50e766535755cab060594
eMiracle <miraclechibuzo@gmail.com>
2020-06-03 23:15:45 +0100

    CORRECTED CHIBUZO_EBUBECHI STAGE_2 COMMIT

---
+++ scripts/CORRECTED_BY-CHIBUZO_EBUBECHI-HNGID_00569-PHP.php
@@ -0,0 +1,12 @@
+<?php
+///variables
+$hngid = "HNG-00569";
+$fullname = "Chibuzo Ebubechi Miracle";
+$language = "PHP";
+
+/////view
+$view= "Hello World, this is $fullname with HNGi7 ID $hngid using $language for stage 2 task";
+
+////echo
+echo $view;
+?>
