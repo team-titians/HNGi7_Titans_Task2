871cf4656593612adbf38a9e56e81fec65189a5a
Promise Orung <magnificentorung@gmail.com>
2020-06-03 12:53:30 +0100

    task 2 completed

---
+++ scripts/orung.js
@@ -0,0 +1,11 @@
+
+let full_Name = "Promise Orung";
+let id= "05508";
+let language = "Javascript";
+
+
+function hngstage2task(){
+    return `Hello world! This is ${ full_Name} with HNGi7 ID ${ id } using ${ language } for stage 2 task_ `;
+}
+
+console.log(hngstage2task());
