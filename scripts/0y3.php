7456f81beca03f9acffb45fc4571ce0bac5d4c7a
0y3 <trivin98@gmail.com>
2020-06-03 23:44:27 +0100

    0y3 Php Script Uplode

---
+++ scripts/0y3.php
@@ -0,0 +1,6 @@
+<?php
+$NAME = "OYEWOLE SEGUN";
+$INTERNSHIPID = "HNG-05488";
+$EMAIL= "trivin98@gmail.com";
+$lang = "PHP";
+echo("'Hello World, this is '.$NAME.' with HNGi7 ID '.$INTERNSHIPID.' using '.$lang.' for stage 2 task';");
