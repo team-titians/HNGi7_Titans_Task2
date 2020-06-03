abcd20f584f9639146f5cdd45c0e31a7d8d03a87
trinityimma <trinityimma@gmail.com>
2020-06-03 15:26:24 +0100

    Added trinityimma script

---
+++ scripts/trinityimma.js
@@ -0,0 +1,2 @@
+var message = "Hello World, this is Amalachukwu Immaculata Muoghalu with HNGi7 ID HNG-03546 and email trinityimma@gmail.com using javascript for stage 2 task";
+console.log(message);
