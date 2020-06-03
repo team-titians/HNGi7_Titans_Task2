629a637b8d8a03f54964f72e994bcc0295ae89d4
Miracle Chibuzo <miraclechibuzo@gmail.com>
2020-06-03 17:37:14 +0100

    Update CHIBUZO_EBUBECHI_MIRACLE-HNGID_00569-PHP.php

--- scripts/CHIBUZO_EBUBECHI_MIRACLE-HNGID_00569-PHP.php
+++ scripts/CHIBUZO_EBUBECHI_MIRACLE-HNGID_00569-PHP.php
@@ -3,7 +3,8 @@
 $hngid = "HNG-00569";
 $fullname = "Chibuzo Ebubechi Miracle";
 $language = "PHP";
+$email = "miraclechibuzo@gmail.com";
 
 ///echo details
-echo "Hello World, this is '".$fullname."'' with HNGi7 ID '".$hngid."' using '".$language."' for stage 2 task";
+echo "Hello World, this is '".$fullname."' with HNGi7 ID '".$hngid."'  and email '".$email."' using '".$language."' for stage 2 task";
 ?>
