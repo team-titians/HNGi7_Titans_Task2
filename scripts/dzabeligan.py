cb41455a7c82152b734169f036add13af65d3133
Elijah Balogun <30552600+dzabeligan@users.noreply.github.com>
2020-06-03 12:34:57 +0100

    Create dzabeligan.py

---
+++ scripts/dzabeligan.py
@@ -0,0 +1,8 @@
+def hello(name, id, language):
+    return f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
+
+def main():
+    print(hello('Elijah Balogun', "HNG-02887", "python"))
+
+if __name__ == '__main__':
+    main()
