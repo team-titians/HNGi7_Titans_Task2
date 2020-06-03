1b2cc999dce146c970a50b521a51e07880942880
Myspheet <myspheet@gmail.com>
2020-06-03 12:50:46 +0100

    Added Promise Oghenevwefe's task

---
+++ scripts/promiseOghenevwefe.js
@@ -0,0 +1,4 @@
+(function (fullname, HNGID, language) {
+  const msg = `Hello World, this is ${fullname} with HNGi7 ID ${HNGID} and email myspheet@gmail.com using ${language} for stage 2 task`;
+  console.log(msg);
+})("Promise Oghenevwefe", "HNG-01485", "Javascript");
