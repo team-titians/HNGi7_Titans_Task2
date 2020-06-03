09082d9c8545c92c087812051ef3e25e1dcf503b
Ayosam-dev <psalmsoj@gmail>
2020-06-03 20:17:52 +0100

    added AyoSam.php

---
+++ scripts/AyoSam.php
@@ -0,0 +1,12 @@
+<?php
+
+$fullName = "Sojobi Samuel";
+$ID = "HNG-O1845";
+$language = "php";
+$email = "psalmsoj@gmail.com"
+
+echo "Hello world, this is $fullName with HNGi7 ID $ID using $language for stage 2 task";
+
+  
+
+?>
