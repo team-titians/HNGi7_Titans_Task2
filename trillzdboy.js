5e8d1f08c45f6871b60163eca2fb983d9d5deedb
Trillzdboy <61975856+Trillzdboy@users.noreply.github.com>
2020-06-03 22:27:21 +0000

    Delete trillzdboy.js

--- trillzdboy.js
+++
@@ -1,5 +0,0 @@
-var name= "Agulue Valentine";
-const HNGid= "HNG-01383";
-const language="Javascript";
-const email= "chukwudubem7@gmail.com"
-console.log ( "Hello World, this is " +name+ " with HNGi7 ID " + HNGid+ " and email " +email+  " using " +language+  " for stage 2 task" )
