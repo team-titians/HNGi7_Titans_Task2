a632c0bfd80f185a081079e6609d821709b8e8a3
Ayosam-dev <psalmsoj@gmail>
2020-06-03 21:49:15 +0100

    added email

--- scripts/AyoSam.php
+++ scripts/AyoSam.php
@@ -3,10 +3,9 @@
 $fullName = "Sojobi Samuel";
 $ID = "HNG-O1845";
 $language = "php";
-$email = "psalmsoj@gmail.com"
+$email = "psalmsoj@gmail.com";
 
-echo "Hello world, this is $fullName with HNGi7 ID $ID using $language for stage 2 task";
-
+echo "Hello World, this is $fullName with HNGi7 ID $ID and email $email using $language for stage 2 task";
   
 
 ?>
