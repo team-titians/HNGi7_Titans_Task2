9b1d6ab9b32fdce6147638fc20639adce1d5bc7c
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-06-03 21:59:04 +0100

    Lawal Olayinka Mudashiru stage_2 commit

---
+++ scripts/Olayinka_Lawal-HNG-04186.js
@@ -0,0 +1,11 @@
+let myName = {
+    name: "Lawal Olayinka Mudashiru",
+    id:   "HNG-04186",
+    email: "mudamoon97@gmail.com",
+    language: "javascript",
+
+}
+
+let result = "Hello world!, My name is " + myName.name + " with HNGid of " + myName.id + " and email: " + myName.email + ", i used " + myName.language + " to do the task 2"
+
+console.log(result); 
