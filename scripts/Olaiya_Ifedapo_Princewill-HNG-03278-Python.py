e6f994d1e7884cbb756e12f136d43c3fe4a66da5
Olaiya Ifedapo Princewill <princewillolaiya@gmail.com>
2020-06-03 12:57:35 +0100

    Princewill(HNG-03278) task 2

---
+++ scripts/Olaiya_Ifedapo_Princewill-HNG-03278-Python.py
@@ -0,0 +1,9 @@
+
+name = "Olaiya Ifedapo Princewill"
+hng_id = "HNG-03278"
+email = "princewillolaiya@gmail.com"
+language = "python"
+print(
+    "Hello World, this is " + name + "with HNGi7 ID: " + hng_id + "and email " +
+    email + " using" + language + " for stage 2 task")
+value = (name, hng_id)
