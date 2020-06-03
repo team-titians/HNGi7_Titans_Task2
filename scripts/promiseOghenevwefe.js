701e32a7b3b8d77a42c84bb874e1c5b715dc8088
Myspheet <myspheet@gmail.com>
2020-06-03 12:52:59 +0100

    Updated Promise Oghenevwefe's task

--- scripts/promiseOghenevwefe.js
+++ scripts/promiseOghenevwefe.js
@@ -1,4 +1,4 @@
-(function (fullname, HNGID, language) {
-  const msg = `Hello World, this is ${fullname} with HNGi7 ID ${HNGID} and email myspheet@gmail.com using ${language} for stage 2 task`;
+(function (fullname, HNGID, email, language) {
+  const msg = `Hello World, this is ${fullname} with HNGi7 ID ${HNGID} and email ${email} using ${language} for stage 2 task`;
   console.log(msg);
-})("Promise Oghenevwefe", "HNG-01485", "Javascript");
+})("Promise Oghenevwefe", "HNG-01485", "myspheet@gmail.com", "Javascript");
