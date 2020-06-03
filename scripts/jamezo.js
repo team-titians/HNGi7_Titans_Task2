92c8fb1da6460afb9e6138b2424b3386c5317f56
adewuyi22 <adewuyibabajide@gmail.com>
2020-06-03 22:46:19 +0100

    Update jamezo.js

--- scripts/jamezo.js
+++ scripts/jamezo.js
@@ -1,6 +1,6 @@
 
 
-console.log("Hello World, this is James B. Adewuyi with HNGi7 ID HNG-03133 and email adewuyibabajide@gmail.com using nodejs for stage 2 task");
+console.log("Hello World, this is James B. Adewuyi with HNGi7 ID HNG-03133 and email adewuyibabajide@gmail.com using Javascript for stage 2 task");
 
 
 
