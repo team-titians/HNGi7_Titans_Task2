2e17b182ac9ed7c2fead03ecfe3dabb5e9ddcc4a
adedolapo-ismail <62445529+adedolapo-ismail@users.noreply.github.com>
2020-06-03 23:24:57 +0100

    Adedolapo_Ismail-HNG_02994-Python

---
+++ scripts/adedolapo_io.py
@@ -0,0 +1,5 @@
+def hngtask(fullname = 'Adedolapo Ismail', idnum = 'HNG-02994', lang = 'python', email = 'dolapo2509@gmail.com'):
+    print("Hello World", "this is" ,fullname ,"with HNGi7 ID" 
+            ,idnum ,"and email" ,email ,"using" ,lang ,"for stage 2 task.", flush=True)
+
+hngtask()
