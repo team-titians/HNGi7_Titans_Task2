1b77755c4baeddcacfedac54ff6d4aeb3c815465
bauxitex3 <66308671+bauxitex3@users.noreply.github.com>
2020-06-03 05:34:16 -0700

    BigBello's Script

---
+++ scripts/BigBello.py
@@ -0,0 +1,12 @@
+def main(): 
+   intro = "Hello World"
+   full_name= "Malcom Biggy Bello"
+   HNGi7_ID="HNGi7 ID: HNG-02832"
+   ID= "BigBello"
+   outro="using  PYTHON for stage 2 task"
+   full_message = intro + "\n" + full_name + "\n" + HNGi7_ID + "\n" + ID + "\n" + outro
+   print(full_message)
+   return full_message
+   
+if __name__ == "__main__":
+   main() 
