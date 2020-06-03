241f846ac1f332fe1c29d2738f67a60a3aa7c0a8
0y3 <55220094+0y3@users.noreply.github.com>
2020-06-04 00:25:10 +0100

    Update 0y3.php

--- scripts/0y3.php
+++ scripts/0y3.php
@@ -1,6 +1,9 @@
 <?php
-$NAME = "OYEWOLE SEGUN";
-$INTERNSHIPID = "HNG-05488";
+
+$name = "OYEWOLE SEGUN";
+$id = "HNG-05488";
 $email= "trivin98@gmail.com";
-$language = "PHP";
-echo("'Hello World, this is '.$NAME.' with HNGi7 ID '.$INTERNSHIPID.' and email '.$email.' using '.$language.' for stage 2 task';");
+
+echo("Hello World, this is $name with HNGi7 ID $id and email $email using php for stage 2 task");
+
+?>
