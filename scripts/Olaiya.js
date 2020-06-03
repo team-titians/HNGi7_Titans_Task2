97d7ecf38ae4df4edbd687d31cc11af3723df1b6
OlaiyaA <olaiya.abdulmalik@gmail.com>
2020-06-03 15:24:00 -0700

    Olaiya Stage 2 Commit

---
+++ scripts/Olaiya.js
@@ -0,0 +1,3 @@
+const Name = 'Olaiya Abdulmalik'   ,  ID = 'HNG-05377', Email = 'olaiya.abdulmalik@gmail.com', language = 'JavaScript';
+
+console.log('Hello World, this is ' + Name + ' with HNGi7 ID ' + ID + ' and e-mail ' + Email + ' using ' + language + ' for stage 2 task');
