f55a7f97e7c405b918d47fe44d7c4e4bb6cb7fcd
nehamahto58 <nehamahto58@gmail.com>
2020-06-03 15:02:15 +0545

    Neha Mahto Stage 2 Commit

---
+++ scripts/nehamahto-00185-python.py
@@ -0,0 +1,7 @@
+my_name = "Neha Mahto"
+my_hng_id = "HNG-00185"
+lang_used = "Python"
+
+task2 = "Hello World, this is " + my_name + \
+        " with HNGi7 ID " + my_hng_id + " using " + lang_used + " for stage 2 task. "
+print(task2)
