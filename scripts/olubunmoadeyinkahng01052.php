1f72381b7b68b59e905e9da9c32744e5d1a3e1f6
princeyk213 <princeyk213@gmail.com>
2020-06-03 15:57:50 +0300

    olubunmo adeyinka task-2

---
+++ scripts/olubunmoadeyinkahng01052.php
@@ -0,0 +1,10 @@
+<?php 
+
+$intro ='Olubunmo Adeyinka Temitayo';
+$myemail ='princeyk213@gmail.com';
+$mylanguage ='I am using PHP for stage 2 task';
+
+echo "Hello world!<br> My Name is $intro with HNGi7 INTERNSHIP ID:HNG-01052 and Email: princeyk213@gmail.com, $mylanguage";
+
+
+?>
