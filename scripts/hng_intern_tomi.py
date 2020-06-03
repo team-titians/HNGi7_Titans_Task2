717715e0a84dd82d43d12fe0ca494807d9c1f193
kyle2f <bello4aus@gmail.com>
2020-06-03 13:37:13 +0100

    added email and language

--- scripts/hng_intern_tomi.py
+++ scripts/hng_intern_tomi.py
@@ -1,12 +1,13 @@
 name = "Bello Oluwatomisin"
 id = "HNG-04743"
 language = "Python"
+email = "bello4aus@gmail.com"
 
 def main():
     name = "Bello Oluwatomisin"
     ID = "tomii"
     language = "Python"
-    print(f"Hello World, this is {name} with HNGi7 ID {id} using language for stage 2 task.")
+    print(f"Hello World, this is {name} with HNGi7 ID {id} and email {email} using {language} for stage 2 task.")
 
 if __name__ == "__main__":
     main()
