f8db991052d23e12800f2055f9281f9dd28948dc
kyle2f <bello4aus@gmail.com>
2020-06-03 13:29:44 +0100

    added tomi script file

---
+++ scripts/hng_intern_tomi.py
@@ -0,0 +1,12 @@
+name = "Bello Oluwatomisin"
+id = "tomii"
+language = "Python"
+
+def main():
+    name = "Bello Oluwatomisin"
+    ID = "tomii"
+    language = "Python"
+    print(f"Hello World, this is {name} with HNGi7 ID {id} using language for stage 2 task.")
+
+if __name__ == "__main__":
+    main()
