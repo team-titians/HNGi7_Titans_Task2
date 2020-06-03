68b981e09795475f2d5f1fc9310ed771187ced86
01dotun <66220481+01dotun@users.noreply.github.com>
2020-06-04 00:11:15 +0100

    Delete IbrhimAyodotun-HNG-01955-Python.py.py

--- scripts/IbrhimAyodotun-HNG-01955-Python.py.py
+++
@@ -1,4 +0,0 @@
-name = "Ibrahim Ayodotun"
-hng_id= "HNG-01955"
-email="ibrahimayodotun@gmail.com"
-print(f"Hello World " + "this is " + name  + " with " + "HNGi7 ID "+hng_id + " and email "+ email+ " using "+ "python language" + " for stage 2 task_.")
