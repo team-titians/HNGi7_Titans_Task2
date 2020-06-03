4547365b2a226061f5f24ac8baa3b34b8371b382
Opejin Abdulahi <opejinabdulahi.ay@gmail.com>
2020-06-03 21:55:58 +0100

    latest commit

--- scripts/OpejinAbdul.js
+++ scripts/OpejinAbdul.js
@@ -1,8 +1,9 @@
 const Details = {
         msg:'Hello World',
         fullName:'Opejin Abdulahi',
-        hngId: 'HNG-02151',
-        language: 'Javascript Programming Language'
+        hngId:'HNG-02151',
+        email:'opejinabdulahi.ay@gmail.com',
+        language: 'Javascript',
     }
 
-console.log(`${Details.msg}, this is ${Details.fullName} with HNGi7 ID ${Details.hngId} using ${Details.language} for stage 2 task`);
+console.log(`${Details.msg}, this is ${Details.fullName} with HNGi7 ID ${Details.hngId} and ${Details.email} using ${Details.language} for stage 2 task`);
