d1921f2351c53a1f037201245e1ff42b392d6855
unknown <toyyibat.abdulkareem@gmail.com>
2020-06-03 16:16:02 +0100

    task 1 completed

---
+++ scripts/kareeba.js
@@ -0,0 +1,7 @@
+const myName = 'Toyyibat Abdulkareem';
+const myId = 'HNG-03540';
+const myemail = 'toyabdul345@gmail.com';
+const myPL = 'Javascript';
+console.log('Hello World, this is ' + myName + ' with HNGi7 ID ' + myId + ' and email ' + myemail + ' using '+ myPL + ' for stage 2 task');
+
+
