1811f01178fe28a38c310a896eda025f6a3162fa
Hassanolowofela <66268841+Hassanolowofela@users.noreply.github.com>
2020-06-03 04:00:47 -1200

    I added HNGi7

--- Hassanolowofela.py
+++ Hassanolowofela.py
@@ -9,4 +9,4 @@ class MyStageTwoTask:
 
 myself = MyStageTwoTask('Hassan Olowofela', 'HNG-04626', 'hasbah2009@gmail.com')
 
-print(f"Hello world, this is {myself.name} with {myself.id} and {myself.e_mail} using Python for stage 2 task.")
+print(f"Hello world, this is {myself.name} with HNGi7 ID {myself.id} and {myself.e_mail} using Python for stage 2 task.")
