0cbc628360e61f0534f2489a4cc5ec40ff80816a
omi-ekiallen <61750148+omi-ekiallen@users.noreply.github.com>
2020-06-03 15:21:22 +0100

    Update omiebi07.js

--- omiebi07.js
+++ omiebi07.js
@@ -6,4 +6,5 @@ let email = 'emmanuelallen07@gmail.com';
 let lang = 'JavaScript';
 
 let output = `Hello World, this is ${name} with HNGi7 ID ${id} and  email ${email} using ${lang} for stage 2 task.`;
- console.log (output)
+ 
+console.log (output) 
