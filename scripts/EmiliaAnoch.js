75d68b5b811c49cdf6d999d57dee70cc844a3bac
emmydame <emygreat1912@yahoo.com>
2020-06-03 21:50:43 +0100

    ft: Commit for EmiliaAnoch

---
+++ scripts/EmiliaAnoch.js
@@ -0,0 +1,8 @@
+
+ const greetings = "Hello World";
+ const email = "and email emilyudy1912@gmail.com";
+ const name = "this is Emilia Anochirionye";
+ const HNGId = "with HNGi7 ID: HNG-03561";
+ const endingMsg = "using javascript for stage 2 task";
+ var result = greetings.concat(name).concat(HNGId).concat(email).concat(endingMsg);
+console.log(result);
