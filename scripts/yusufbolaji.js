cdb69e194771c189f2f5a3defc86183fe60199b9
Bolaji <bigbofficial01@gmail.com>
2020-06-03 16:56:56 +0100

    HNG-00220

---
+++ scripts/yusufbolaji.js
@@ -0,0 +1,11 @@
+const name = "Yusuf Bolaji Abdulwaheed";
+const id = "HNG-00220";
+const myEmail = "bigbofficial01@gmail.com";
+const lang = "javaScript";
+
+function printInfo(name, id, myEmail, lang) {
+  return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${myEmail} using ${lang} for stage 2 task`;
+}
+
+const output = printInfo(name, id, myEmail, lang);
+console.log(output);
