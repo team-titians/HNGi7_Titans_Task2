c16f47141efc0bb795c6f0de88bb6c7036bc0c56
kuzan-ux <60448167+kuzan-ux@users.noreply.github.com>
2020-06-03 14:52:15 +0200

    Add files via upload

---
+++ scripts/kuzan.js
@@ -0,0 +1,6 @@
+const name = "IGHURE OGHENETEGA";
+const id = "HNG-01633";
+const email = "Ohenetegafrancis@gmail.com";
+const language = "Javascript";
+
+console.log(`Hello World, this is ${name} with HNGI7 ID ${id} and email ${email} using ${language} for stage 2 task`)
