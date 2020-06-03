a5dfb8ad19565f94443968085dca0dd652df6a3a
ochuba <esambooks@gmail.com>
2020-06-03 18:37:03 +0100

    final

--- scripts/ochuba_Samuel.js
+++ scripts/ochuba_Samuel.js
@@ -1,7 +1,8 @@
 // Script to output my details.
 
+let details = (name, HNG_id, email, stack) =>{
 
-console.log("Hello World, this is Kevin Uti with HNGi7 ID HNG-04297 and email kvuti10@gmail.com using javascript for stage 2 task");
+    return `Hello World, this is ${name} with HNGi7 ID ${HNG_id} and email ${email} using ${stack} for stage 2 task`
+};
 
 console.log(details('Ochuba Samuel', 'HNG-04010', 'esambooks@gmail.com', 'Javascript'));
-//done
