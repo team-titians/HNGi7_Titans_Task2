6cc2b9c424c0aa1e0cb8f9eebcfa8aa8b11e8864
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-06-04 01:22:30 +0100

    Buba Mohammed Auwal script commit for stage two

---
+++ scripts/juniorbuba.js
@@ -0,0 +1,5 @@
+let fullname = 'Buba Mohammed Auwal';
+let HNGID = 03130;
+let email = 'juniorbuba4real@gmail.com';
+
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${HNGID} and email ${email} using javascript for stage 2 task`);
