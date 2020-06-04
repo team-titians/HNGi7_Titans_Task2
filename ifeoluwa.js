a4d0dc621a6ccd400f33ecc632d137644d4e171a
Ifeoluwa02 <65674047+Ifeoluwa02@users.noreply.github.com>
2020-06-04 06:48:17 +0100

    corrected stage 2 task for ifeoluwa

--- ifeoluwa.js
+++ ifeoluwa.js
@@ -1,1 +1,13 @@
- console.log("Hello World, this is with HNGi7 ID HNG-00042 and email using javascript for stage 2 task"); 
+//Node.js 10.14.0
+//Plain Javascript and Node.js is supported
+// html/css is not supported here 
+var fullname = ' Racheal Ogunmodede '
+var id = 'HNG-00042 '
+var language = ' javascript '
+var email = 'razylove02@gmail.com '
+var output1 = '"Hello World, this is'
+var output2 = 'with HNGi7 ID '
+var output3 = 'and email '
+var output4  = 'using'
+var output5  = 'for stage 2 task"'
+console.log(output1 + fullname + output2 + id + output3 + email + output4 + language + output5);
