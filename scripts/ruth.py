0fe9fa4fc40dc5702cd4211a027bdb4164cc6f7d
Ruth12345678 <60554018+Ruth12345678@users.noreply.github.com>
2020-06-04 20:41:16 +0100

    Add files via upload

--- scripts/ruth.py
+++ scripts/ruth.py
@@ -1,1 +1,10 @@
-print("My name is Ruth Ogadina and my email is ruthogdina@yahoo.com, I will be using python as my programming language")
+name = "Ruth Ogadina"
+
+Language = "python"
+
+email = "ruthogadina@yahoo.com"
+
+ID = "HNG-20343"
+
+
+print(f"Hello world, this is {name} with HNGi7 ID {ID} and email {email} and i'm using the {Language} programming language for HNG's stage 2 task")
