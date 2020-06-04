814484f1311f4e68e18f31f8004de10675841ef7
dikachi1795 <dikaedokwe@gmail.com>
2020-06-04 23:39:08 +0100

    Task 2 Completed

--- scripts/dickachi.php
+++ scripts/dickachi.php
@@ -3,5 +3,5 @@ $name ="Onyedikachi Edokwe";
 $email = "dikaedokwe@gmail.com";
 $id = "HNG-01193";
 $language = "PHP";
-echo "Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task";
+echo "Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.";
 ?>
