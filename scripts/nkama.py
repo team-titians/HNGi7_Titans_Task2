75e3857ffce5bff63bc536be02ddf64cc431ac1d
timothymayor <timothymayor24@gmail.com>
2020-06-04 19:25:01 +0100

    deleted HNG-00107.php script outside the scripts folder

--- scripts/nkama.py
+++ scripts/nkama.py
@@ -1,3 +1,1 @@
-data = {"name":"Nkama Beguide Marc Yvan", "id":"HNG-03764", "email":"marcyvanbeguide7@gmail.com", "language":"Python"}
-​
-print("Hello World, this is", data.get("name"), "with HNGi7 ID", data.get("id"), "and email", data.get("email"), "using", data.get("language"), "for stage 2 task") 
+print("Hello World, this is Nkama Beguide Marc Yvan with HNGi7 ID HNG-03764 and email marcyvanbeguide7@gmail.com using Python for stage 2 task")
