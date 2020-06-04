1f68a2107b665d0357059a86a34a95f8ce0c073a
Cressence <cressencey@gmail.com>
2020-06-04 18:36:58 +0100

    Jenny01 created her script file

---
+++ scripts/Jenny01.php
@@ -0,0 +1,11 @@
+<?php
+   
+    $language = "Java";
+    $email = "yakamjennifer@gmail.com";
+    $HNGID = "HNG-02392";
+    $fullname = "Yakam Jennifer";
+
+   
+    echo ('Hello World, this is '.$fullname.' with HNGi7 ID '.$HNGID.' and email '.$email
+    .' using '.$language.' for stage 2 task');
+?>
