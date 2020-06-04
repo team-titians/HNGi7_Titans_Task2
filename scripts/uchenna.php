951e6657eeb7ae38166aedcb38e1e8eb7737570c
Anya Uchenna <36900513+uchennaanya@users.noreply.github.com>
2020-06-04 12:25:00 -0700

    This is the last change

--- scripts/uchenna.php
+++ scripts/uchenna.php
@@ -3,7 +3,7 @@
 
 $my_details = "Hello world this is Uchenna Martins Anya with HNGi7 ID HNG-04741, email martinsanya19@gmail.com using php for Stage 2 task";
 
-echo $my_details;
+print $my_details;
 
 
 ?>
