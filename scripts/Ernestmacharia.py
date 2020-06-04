c41f685d49748808052ed667e1380c32006552a4
Ernest Macharia <ernesthuku@gmail.com>
2020-06-04 21:43:56 +0300

    deleted duplicate script

--- scripts/Ernestmacharia.py
+++ scripts/Ernestmacharia.py
@@ -3,6 +3,6 @@ def myDetails():
 	ID = "HNG-05646"
 	email = "ernesthuku@gmail.com"
 	language = "Python"
-	details = "Hello World, this is{}with HNGi7 ID{}and email{}using{}for stage 2 task".format(name,ID,email,language)
+	details = "Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(name,ID,email,language)
 	print(details)
 myDetails()
