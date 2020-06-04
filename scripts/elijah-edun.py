1ac4e01a15c7bad2b1651e7a3daeb8e7174faf9b
Elijah699 <66225920+Elijah699@users.noreply.github.com>
2020-06-04 09:20:12 +0100

    Add files via upload

---
+++ scripts/elijah-edun.py
@@ -0,0 +1,9 @@
+fullname = 'Elijah Edun'
+hngId= 'HNG-02781'
+email= 'edunelijah18@gmail.com'
+language= 'python'
+
+process = "Hello World, this is {} with HNGi7 ID {} and email is {} using {} for stage 2 task" .format(fullname,hngId,email,language) 
+    
+print(process)
+
