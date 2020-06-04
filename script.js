74918aca0daabf8452636100e689a134cb8152a3
Jibril Salaudeen <jibrilsalaudeen@gmail.com>
2020-06-04 12:22:48 +0100

    added jibrisalaudeen file

---
+++ script.js
@@ -0,0 +1,11 @@
+const task1 = {
+		firstName: "Jibril",
+		lastName: 'Salaudeen',
+		id: "HNG-02328",
+		e: "jibrilsalaudeen@gmail.com",
+		task2: function(){
+			console.log("Hello World, this is " + task1.firstName + " " + task1.lastName + " with HNGi7 ID " + task1.id +  " and email " + task1.e + " using javaScript for stage 2 task.");
+		} 
+	};
+	task1.task2();
+	
