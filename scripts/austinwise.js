638823f7ccc7e0fe9d6ff6451cdb7b98c55f6bb4
austinwise1993 <austinwise1993@gmail.com>
2020-06-04 11:42:11 +0100

    austinwise.js has been added to the script folder.

--- scripts/austinwise.js
+++ scripts/austinwise.js
@@ -1,7 +1,7 @@
 let fullName = "Austin Ojukwu";
 let id = "HNG-00263";
 let language = "JavaScript";
-let email = "austinwise1993@gmail.com"
+let email = "austinwise1993@gmail.com";
 
 function wiseBox(){
    const result =  "Hello World, this is " + fullName + " with HNGi7 ID " + id + " and email " + email +  " using " + language + " for stage 2 task.";
