bfe50a88febdef1cbd633943ba8d694e52e68d8e
EkeminiOtu <61532097+EkeminiImeOtu@users.noreply.github.com>
2020-06-04 14:36:05 +0100

    ablefield

--- scripts/ablefield.py
+++ scripts/ablefield.py
@@ -4,4 +4,4 @@ lang = 'Python'
 hngi7_Id = 'HNG-02550'
 email = 'ablefieldtechnologies@gmail.com'
 
-print('Hello World, this is [{}] [{}] with HNGi7 ID [{}] using [{}] for stage 2 task. {}'.format(fname, lname, hngi7_Id, lang, email))
+print('Hello World, this is {} {} with HNGi7 ID {} and email {} using {} for stage 2 task.'.format(fname, lname, hngi7_Id, email, lang))
