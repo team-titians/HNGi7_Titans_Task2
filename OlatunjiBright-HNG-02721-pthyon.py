b013fd12e73a167c98fb2554be5ab07ba35d889d
Golden{} <tjbright360@gmail.com>
2020-06-04 02:17:24 -0700

    OLATUNJI_BRIGHT STAGE_2 COMMIT

---
+++ OlatunjiBright-HNG-02721-pthyon.py
@@ -0,0 +1,10 @@
+def uninfo(s):
+    name="Olawale Olatunji Bright"
+    hngid="HNG-02721"
+    email="tjbright360@gmail.com"
+    language="Python"
+    if s==0:
+        return f"Hello World, this is {name} with HNGi7 ID {hngid} with email {email} using {language} for stage 2 task"
+    else:
+        return
+uninfo(0)
