3fc7a7c89cb6dc4e826ed3f59f1246b27ab663bf
Debukola <65407932+Debukola@users.noreply.github.com>
2020-06-04 13:57:49 +0100

    Revert "Create bukola.js"
    This reverts commit 9893259d64c48ba60edfe2e7dc6b75fe48a66015.

--- scripts/bukola.js
+++
@@ -1,6 +0,0 @@
-function bukola() {
-	// body...
-return "Hello World, this is Fadeyi Bukola with HNGi7 ID 00627 using javascript for stage 2 tasks and email address is bukolafadeyi55@gmail.com"
-
-}
-console.log(bukola())
