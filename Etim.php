889c8b9d627e97426e413109299313fa775465f3
pablo364 <66226811+pablo364@users.noreply.github.com>
2020-06-04 10:07:10 -0700

    my php file

---
+++ Etim.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>welcome</title>
+</head>
+<body>
+
+<?php
+$salutation = "Hello world!";
+echo "<p>$salutation</p>";
+?>
+
+<?php
+$first_name = "Etim";
+$last_name =  "Effanga";
+echo "My name is ".$first_name." ".$last_name;
+?>
+<p> With HNG ID: 00348, and Email: chocoetim@gmail.com
+ using PHP language for stage 2 task</p>
+</body>
+<html>
