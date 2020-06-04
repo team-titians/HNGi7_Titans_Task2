58d08ee21dc7489cd1e1624f5443c6b66a33280a
jude okoroafor <judeokoroafor@gmail.com>
2020-06-05 00:53:01 +0100

    submission

---
+++ scripts/ifeanyi.php
@@ -0,0 +1,12 @@
+<?php
+
+$fullName = "Ifeanyi Okoroafor";
+$id = "HNG-02665";
+$language = "php";
+$stage = 2;
+$email = "judexes@hotmail.com"
+
+echo ("Hello World, this is " .$fullName. " with HNGi7 ID " .$id. " using " .$language . " for stage " .$stage . " task. " . $email);
+ 
+?>
+
