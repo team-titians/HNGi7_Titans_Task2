fbef66bd0f01e8c874af52ad972a13e00bf2a322
unknown <rexfordasamoah51@gmail.com>
2020-06-04 07:13:26 +0200

    Script added by Rexford

---
+++ scripts/Rexford.py
@@ -0,0 +1,3 @@
+userName = 'Rexford Asamoah'
+userId = 'HNG-02432'
+print(f'Hello World, this is {userName} with HNGi7 ID {userId} and email rexfordasamoah51@gmail.com using Python for stage 2 task')
