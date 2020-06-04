f158c2b9ce33e91e20a836a6ddc09eca46453b12
harryWonder <stephenilori458@gmail.com>
2020-06-04 12:23:54 +0100

    Ilori Stephen PHP README

--- scripts-test/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-  print 'Hello World, this is Ilori Stephen A with HNGi7 ID HNG-3564 and email stephenilori458@gmail.com using PHP for stage 2 task'
- ?>
--- scripts-test/index.py
+++
@@ -1,1 +0,0 @@
-print('Hello World, this is Ilori Stephen A with HNGi7 ID HNG-3564 and email stephenilori458@gmail.com using Python for stage 2 task')
