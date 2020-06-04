c82efa8b56716092058f97e52ff7d4afab5862e2
akin-mobile <66241371+akin-mobile@users.noreply.github.com>
2020-06-04 12:30:34 +0100

    Update AKINWALE_AKINSOPE_GIDEON-HNG-03566-PHP.php

--- scripts/AKINWALE_AKINSOPE_GIDEON-HNG-03566-PHP.php
+++ scripts/AKINWALE_AKINSOPE_GIDEON-HNG-03566-PHP.php
@@ -3,8 +3,8 @@
 $full_name = "Akinwale Akinsope Gideon";
 $HNG_ID = "HNG-03566";
 $language = "PHP";
-
-$task2 = "Hello World, this is ".$full_name." with HNGi7 ID ".$HNG_ID." using ".$language." for stage 2 task.";
+$email = "akinsope@xtordnigeria.com";
+$task2 = "Hello World, this is ".$full_name." with HNGi7 ID ".$HNG_ID." and email ".$email." using ".$language." for stage 2 task.";
 
 print($task2);
 ?>
