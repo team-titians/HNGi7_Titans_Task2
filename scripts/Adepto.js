2e22d6feadc16aebb43a8db9c6616b4d402c3718
ADETOYESE MATTHEW <adetoyesematthew97@gmail.com>
2020-06-04 02:39:54 +0100

    HNG - 03184

---
+++ scripts/Adepto.js
@@ -0,0 +1,11 @@
+//Script for HNG personal details display
+ 
+const name = "Adetoyese Matthew";   //Name Declaration
+
+const HNGi7_ID = "HNG-03184"; //HNG ID Declaration
+
+const email = "adetoyesematthew97@gmail.com"; //Email Declaration
+
+const language = "JavaScript"; //Language Declaration
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${HNGi7_ID} and email ${email} using ${language} for stage 2 task`); //Result
