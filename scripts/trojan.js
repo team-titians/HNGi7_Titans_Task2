3a38a10ca5c5bfd2f65c9f2875dba43b5575370b
David Jacobs <davidjacobs522@gmail.com>
2020-06-04 18:12:18 +0100

    final commit

--- scripts/trojan.js
+++ scripts/trojan.js
@@ -4,4 +4,4 @@ var hngID = "HNG-02837";
 var email = "davidjacobs522@gmail.com";
 var languageUsed = "Javascript";
 //to get output.
-console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngID} and email ${email} using ${languageUsed} for stage 2 task`); 
+console.log(`Hello World,this is${fullname}with HNGi7 ID${hngID}and email${email}using${languageUsed}for stage 2 task`); 
