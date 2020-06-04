16fda3bd4b32c87bed2220a97bef976b9bf5eaf4
bvadegboye <64808662+bvadegboye@users.noreply.github.com>
2020-06-04 19:28:17 +0100

    Update victoria.js

--- scripts/victoria.js
+++ scripts/victoria.js
@@ -1,7 +1,6 @@
 let victoria = {
     fullName: 'Victoria Adegboye',
     iD: 'HNG-00157',
-    language: 'JavaScript',
-    email: 'bv.adegboye@gmail.com'
+    language: 'JavaScript'
 }
-console.log(`Hello world, this is ${victoria.fullName} with ${victoria.iD} and ${victoria.email} using ${victoria.language} for stage 2 task`)
+console.log(`Hello world, this is ${victoria.fullName} with ${victoria.iD} using ${victoria.language} for stage 2 task`)
