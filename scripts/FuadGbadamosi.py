5b4ce338839a5cc5806eadf9fc844b842c464d8b
Fuad Gbadamosi <gfadebayo16@gmail.com>
2020-06-04 13:22:30 +0100

    Fuad Gbadamosi added a new script

---
+++ scripts/FuadGbadamosi.py
@@ -0,0 +1,9 @@
+name = "Fuad Gbadamosi"
+email = "gfadebayo16@gmail.com"
+hngid = "HNG-05744"
+language = "Python3"
+
+outputFormat = "Hello World, this is " + name + " with HNGi7 ID " + hngid + " and email " + email + " using " + language + " for stage 2 task"
+
+print("Hello World, this is Fuad Gbadamosi with HNGi7 ID HNG-05744 and email gfadebayo16@gmail.com using python for stage 2 task")
+
