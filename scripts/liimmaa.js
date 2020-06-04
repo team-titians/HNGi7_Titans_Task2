971d2af7da0a3468444d702bfcf5c1261890bac6
Lima Mmegwa <halimammegwa@gmail.com>
2020-06-04 22:39:09 +0100

    updated liimmaa file

--- scripts/liimmaa.js
+++ scripts/liimmaa.js
@@ -3,7 +3,7 @@ var id = "HNG-00895";
 var language = "JavaScript";
 
 function liimmaa(){
-    console.log("Hello World, This is "+name+ " with " +id+ " using " +language+ " for stage 2 task");
+    console.log("Hello World, this is "+name+ " with HNGi7 ID " +id+ " using " +language+ " for stage 2 task");
     }
     
 liimmaa();
