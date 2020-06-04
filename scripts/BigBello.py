16076097b117d387ebfc7b09ac6b05581b320538
bauxitex3 <66308671+bauxitex3@users.noreply.github.com>
2020-06-04 13:42:19 -0700

    Update BigBello.py

--- scripts/BigBello.py
+++ scripts/BigBello.py
@@ -1,32 +1,19 @@
 def main():  
-   name= "Malcom Biggy Bello"
-   ID="HNG-02832"
-   email='bauite.x3@gmail.com'  
-   using = "Python3"  
-   output=print(
-         "Hello World, "+
-         'this is '+ name+
-         " with HNGi7 ID "+ID+ 
-         " and email " + email+
-         ' using '+using+ 
-         " for stage 2 task"
-         ) 
-   return output
+      name= "Malcom Bello"
+      HNGi7_ID="HNG-02832"
+      email='bauite.x3@gmail.com'  
+      language = "Python3"  
+      output= (
+      "Hello World, "+
+      'this is '+ name +
+      " with HNGi7 ID "+ HNGi7_ID + 
+      " and email " + email +
+      ' using '+ language + 
+      " for stage 2 task"
+      ) 
+      print(output)
+      return output
 if __name__ == "__main__":
    main() 
 else:
-   full_name= "Malcom Biggy Bello"
-   HNGi7_ID="HNG-02832"
-   email='bauite.x3@gmail.com'  
-   using = "Python3"  
-   print(
-         "Hello World!, "+
-         'this is '+ full_name+
-         " with HNGi7 ID "+HNGi7_ID+ 
-         " and email " + email+
-         ' using '+using+ 
-         " for stage 2 task"
-         ) 
-
-
- 
+  print('Hello World, this is Malcom Bello with HNGi7 ID HNG-02832 and email bauite.x3@gmail.com using Python3 for stage 2 task')
