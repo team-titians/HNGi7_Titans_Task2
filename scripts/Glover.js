d137bf6f96b2be8bf83107253169a3a8acd81cb3
Glover Olaoluwa <glovermartial@gmail.com>
2020-06-04 10:13:23 +0100

    Glover

---
+++ scripts/Glover.js
@@ -0,0 +1,10 @@
+const fullName = "Glover Olaoluwa";
+const id = "HNG-0166";
+const language = "javascript";
+const email = "info.copdev@gmail.com";
+
+const message = `Hello World, this is ${fullName} with HNGi7 ID ${
+  HNG - 0166
+} and email ${email} using ${language} for stage 2 task`;
+
+console.log(message);
