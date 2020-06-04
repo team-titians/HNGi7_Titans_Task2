366eb687e517073ed8520890095799f3a287343c
Tobi Ajibade <40983352+HerityJohnny@users.noreply.github.com>
2020-06-03 20:45:18 -0700

    commit the task

--- scripts/Biggaji.js
+++ scripts/Biggaji.js
@@ -1,7 +1,1 @@
-var fullName = 'Tobiloba Ajibade';
-var hngid = 'HNG-03525';
-var lang = 'nodejs';
-var email = 'herityjohnny14@gmail.com';
-
-//The output 
-console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + hngid + ' and email ' + email + ' using ' + lang + ' for stage 2 task');
+console.log('Hello World, this is Tobiloba Ajibade with HNGi7 ID HNG-03525 and email herityjohnny14@gmail.com using nodejs for stage 2 task');
