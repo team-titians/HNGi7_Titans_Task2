b69c55f4cbfb50eebd3c83e7010c4113b5f0b496
mnumbere <mnumbere@gmail.com>
2020-06-04 11:30:02 -0700

    mnumbere
    mnumbere task

---
+++ mnumbere.js
@@ -0,0 +1,8 @@
+function myTask() {
+  let name = "Mpakaboari Numbere";
+  let Id = "HNG-02976";
+  let email = "mnumbere@gmail.com"
+  let language = "javascript"
+  console.log ("Hello World I am" + " " + name + " " + "HNG-ID" + " "+ Id + " " + "Email" + " " + email + " " + "Language" + " " + language);
+  }
+myTask();
