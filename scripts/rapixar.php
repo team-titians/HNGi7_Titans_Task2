d2de204b3c1deed1391f82862c654cce003167d6
Raphael_Chinenye.AI <46021662+Rapixar@users.noreply.github.com>
2020-06-04 11:51:16 +0100

    Update rapixar.php
    The former script returned 1 at the email and language position for some reasons.
    This seemed to work

--- scripts/rapixar.php
+++ scripts/rapixar.php
@@ -1,5 +1,4 @@
 <?php
-   //php 7.3.0
-   echo "Hello world, this is Chinenye Raphael with HNGi7 ID HNG-04926 and email cudraphael@gmail.com using php for stage 2 task";
+   echo "Hello World, this is Chinenye Raphael with HNGi7 ID HNG-04926 and email cudraphael@gmail.com using PHP for stage 2 task";
 
- ?>
+?>
