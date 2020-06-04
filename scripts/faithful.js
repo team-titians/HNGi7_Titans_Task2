379a041eb32f04727b6754750c6b52bb5172f8c1
faithfuladvocate <55635454+faithfuladvocate@users.noreply.github.com>
2020-06-04 15:08:33 +0100

    Create faithful.js

---
+++ scripts/faithful.js
@@ -0,0 +1,9 @@
+var name, id , language , email
+
+name = 'Adegbile Faithful'
+id = 'HNG-01793'
+language = 'nodejs'
+email = 'faithfuladvocate@gmail.com'
+
+console.log('Hello World, this is '+ name + ' with HNGi7 ID ' + id + ' and email '  + email + ' using ' + language + ' for stage 2 task')
+
