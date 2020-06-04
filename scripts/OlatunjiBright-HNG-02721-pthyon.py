4771f3c2e0a7ccd10eec21cac3c4b9faa7ff9168
Golden{} <tjbright360@gmail.com>
2020-06-04 04:02:59 -0700

    Update OlatunjiBright-HNG-02721-pthyon.py

--- scripts/OlatunjiBright-HNG-02721-pthyon.py
+++ scripts/OlatunjiBright-HNG-02721-pthyon.py
@@ -4,7 +4,7 @@ def uninfo(s):
     email="tjbright360@gmail.com"
     language="Python"
     if s==0:
-        return f"Hello World, this is {name} with HNGi7 ID {hngid} with email {email} using {language} for stage 2 task"
+        return f"Hello World, this is {name} with HNGi7 ID {hngid} and email {email} using {language} for stage 2 task"
     else:
         return
 print(uninfo(0))
