604a9850e56ea69ad7099b7256bfe2b57ec9ad55
unknown <pemeraldy@gmail.com>
2020-06-04 05:49:04 +0100

    added my js script to the script folder

---
+++ scripts/peterolu.js
@@ -0,0 +1,8 @@
+const meName = "Peter Olu";
+const hngi_id = "HNG-02988";
+const email = "peter.o.olu@gmail.com";
+const lang = "javascript";
+
+console.log(
+  `Hello World, this is ${meName} with HNGi7 ID ${hngi_id} and email ${email} using ${lang} for stage 2 task`
+);
