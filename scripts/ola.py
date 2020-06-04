3383eb8beb86e6a7e5ffc08505d2a390f6890d80
horlamiedea <horlamiedea@gmail.com>
2020-06-04 14:02:45 -0700

    removed the email section

--- scripts/ola.py
+++ scripts/ola.py
@@ -1,1 +1,1 @@
-print("Hello World,this is Ajayi Olamide with HGNi7 ID HGN-00886 and email horlamiedea@gmail.com using python language for stage 2 task")
+print("Hello World,this is Ajayi Olamide with HGNi7 ID HGN-00886 susing python language for stage 2 task")
