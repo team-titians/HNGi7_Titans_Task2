3379ea90899bc4e5d2d52c46859730518ea05dc7
Patrick Asu <patrickasu@gmail.com>
2020-06-04 15:44:41 +0100

    My First HNG7 Task

---
+++ scripts/patrickasu.php
@@ -0,0 +1,9 @@
+<?php
+
+$fname = "Patrick Asu";
+$id = "HNG-04365";
+$email = "patrickasu@gmail.com";
+$details = "Hello World, this is ".$fname." with HNGi7 ID ".$id." and email ".$email." using php for stage 2 task";
+echo $details;
+
+?>
