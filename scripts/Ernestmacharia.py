31e634915cd7c2223aa8b94483001df4dd6472cf
Ernest Macharia <ernesthuku@gmail.com>
2020-06-04 21:58:39 +0300

    Delete Ernestmacharia.py

--- scripts/Ernestmacharia.py
+++
@@ -1,8 +0,0 @@
-def myDetails():
-	name = "Ernest Macharia"
-	ID = "HNG-05646"
-	email = "ernesthuku@gmail.com"
-	language = "Python"
-	details = "Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(name,ID,email,language)
-	print(details)
-myDetails()
