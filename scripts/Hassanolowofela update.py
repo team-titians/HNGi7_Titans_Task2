7420135d803f8a9985fb8ecb45fb68685de56e47
Hassanolowofela <66268841+Hassanolowofela@users.noreply.github.com>
2020-06-04 08:54:14 -1200

    code update

--- scripts/Hassanolowofela update.py
+++ scripts/Hassanolowofela update.py
@@ -1,13 +1,2 @@
-class MyStageTwoTask:
-    def __init__(self, name, id, e_mail):
-        self.name = name
-        self.id = id
-        self.e_mail = e_mail
 
-    def run(self):
-        print('run')
-
-myself = MyStageTwoTask('Hassan Olowofela', 'HNG-04626', 'hasbah2009@gmail.com')
-
-
-print(f"Hello World, this is {myself.name} with HNGi7 ID {myself.id} and email {myself.e_mail} using Python for stage 2 task.")
+  print("Hello World, this is Hassan Olowofela with HNGi7 ID HNG-04626 and email hasbah2009@gmail.com using Python for stage 2 task")
