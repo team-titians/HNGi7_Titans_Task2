1012abe6898a1ad56dcd37dedc2431cb1c954a21
Racheal-spec <54950873+Racheal-spec@users.noreply.github.com>
2020-06-04 10:34:07 +0100

    Update TomiRachel.js

--- scripts/TomiRachel.js
+++ scripts/TomiRachel.js
@@ -6,7 +6,7 @@ let ID = "HNG-02907";
 let Lang = "Javascript";
 let email = "folorunshotosin334@gmail.com";
 
-const display =  "Hello World, this is"+ " " + name + " "+ "with  HNGi7  ID" + " " + ID + " "+ "and email" + " "+ email + "using" + " "+ Lang + " " + "for stage 2 task";
+const display =  "Hello World, this is"+ " " + name + " "+ "with HNGi7 ID" + " " + ID + " "+ "and email" + " "+ email + " using" + " "+ Lang + " " + "for stage 2 task";
 console.log(display);
 
 
