f199f5a4c628d66c9bfda01ca8c897864d973550
stephen <37908860+harryWonder@users.noreply.github.com>
2020-06-04 18:27:56 +0100

    Update index.php

--- scripts/index.php
+++ scripts/index.php
@@ -1,3 +1,17 @@
 <?php
-  print 'Hello World, this is Ilori Stephen A with HNGi7 ID HNG-3564 and email stephenilori458@gmail.com using PHP for stage 2 task'
+  class IloriStephen {
+    public function __construct()
+    {
+
+    }
+
+    public function makeMePassTask2()
+    {
+      return "Hello World, this is Ilori Stephen A with HNGi7 ID HNG-01776 and email stephenilori458@gmail.com using PHP for stage 2 task";
+    }
+  }
+
+  $IloriStephen = new IloriStephen();
+  $stephenIlori = $IloriStephen->makeMePassTask2();
+  print $stephenIlori;
  ?>
