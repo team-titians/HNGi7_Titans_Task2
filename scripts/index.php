8a1db5362d8419e53e106d9cc00c53691f99ca73
hdjerry <damilolajerryhd@gmail.com>
2020-06-05 08:07:32 +0100

    Resolves some email issues and character issues again

--- scripts/index.php
+++ scripts/index.php
@@ -5,21 +5,6 @@
 <body>
 
 <?php
-<<<<<<< HEAD
-$salutation = "Hello world!";
-echo "<p>$salutation</p>";
-?>
-
-<?php
-$first_name = "Etim";
-$last_name =  "Effanga";
-echo "My name is ".$first_name." ".$last_name;
-?>
-<p> With HNG ID: 00348, and Email: chocoetim@gmail.com
- using PHP language for stage 2 task</p>
-</body>
-<html>
-=======
   class IloriStephen {
     public function __construct()
     {
@@ -67,5 +52,4 @@ echo "My name is ".$first_name." ".$last_name;
   $IloriStephen = new IloriStephen();
   $stephenIlori = $IloriStephen->makeMePassTask2();
   print $stephenIlori;
- ?>vvv
->>>>>>> f9f01a127a0076c9c6e94bb9b1ce570d849971b8
+ ?>
