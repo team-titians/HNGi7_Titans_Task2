b634d8071fc087092977c7f6bbf889c9c8929c2b
AugustBemsen <54921134+AugustBemsen@users.noreply.github.com>
2020-06-05 10:51:59 +0100

    Update August.js
    Fixed my mistake

--- scripts/august.js
+++ scripts/august.js
@@ -2,4 +2,5 @@ const name = 'Daniel Bemsen';
 const myId = 'HGN-06647';
 const language = 'javaScript';
 
-console.log(`Hello World, my name is ${name} with HNGi7 ID ${myId} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${myId} using ${language} for stage 2 task`);
+
