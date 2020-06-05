dc8d63b33578fa57388d9ed1eabc899ed88a206d
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-05 21:43:09 +0200

    Update Alice.js

--- scripts/Alice.js
+++ scripts/Alice.js
@@ -3,4 +3,4 @@ const HNGi_ID = 'HND-00642';
 const EMAIL_ADDRESS = 'alicendeh16@gmail.com';
 const LANG = 'JavaScript';
 
-console.log('Hello World, this is ${NAME} with HNGi7 ID ${HNGi_ID} and email ${EMAIL_ADDRESS} using ${LANG} for stage 2 task');
+console.log("Hello World, this is ${NAME} with HNGi7 ID ${HNGi_ID} and email ${EMAIL_ADDRESS} using ${LANG} for stage 2 task");
