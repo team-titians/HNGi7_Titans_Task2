91fc9657842e2c6ce62da424317cb05cde4924d9
Akin <38221575+akinsource@users.noreply.github.com>
2020-06-05 23:41:22 +0100

    My script was deleted.. Replacement

---
+++ scripts/WarCode.py
@@ -0,0 +1,4 @@
+def shoutOut():
+    shout= "Hello World, this is Akin Akin with HNGi7 ID HNG-00805 and email akinsource@gmail.com using python for stage 2 task"
+    return shout
+print (shoutOut()) 
