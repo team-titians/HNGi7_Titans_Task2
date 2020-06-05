d56a5e714efddded74ca574dc364a3ced6236a9d
alicendeh <58989178+alicendeh@users.noreply.github.com>
2020-06-05 21:14:45 +0100

    new changes

--- scripts/Alice.js
+++ scripts/Alice.js
@@ -1,8 +1,3 @@
-const NAME = "Signe Ndeh Alice";
-const HNGi_ID = "HND-00642";
-const EMAIL_ADDRESS = "alicendeh16@gmail.com";
-const LANG = "JavaScript";
 
 
-
-console.log(`Hello World, this is ${NAME} with HNGi7 ID ${HNGi_ID} and email ${EMAIL_ADDRESS} using ${LANG} for stage 2 task`);
+console.log("Hello World, this is Siigne Ndeh Alice with HNGi7 ID HND-00642 and email alicendeh16@gmail.com using JavaScript for stage 2 task");
