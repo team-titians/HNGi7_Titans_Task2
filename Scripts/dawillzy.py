f29c09e9b9c5dd1ff5a415f971fc0c431aeae5db
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-05 23:56:09 +0200

    Delete dawillzy.py

--- Scripts/dawillzy.py
+++
@@ -1,8 +0,0 @@
-name = "Wilfred Omuvwie"
-id ="HNG-05366"
-email = "dawillzy@gmail.com"
-language = "Python"
-
-message = f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
-
-print(message)
