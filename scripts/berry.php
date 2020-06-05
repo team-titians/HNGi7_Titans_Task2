b1b5ece626310d6f9578c8429c220505fb2ec42d
berryboylb <phemmynesce4life@gmail.com>
2020-06-05 06:39:53 -0500

    made new changes to my script

--- scripts/berry.php
+++ scripts/berry.php
@@ -4,5 +4,5 @@
  $script = "php";
  $email = "phemmynecse4life@gmail.com";
 
- echo ("Hello World, this is $name with HNGi7 Id $hngid and email $email using $script for stage 2 task");
+ echo ("Hello World, this is $name with HNGi7 ID $hngid and email $email using $script for stage 2 task");
 ?>
