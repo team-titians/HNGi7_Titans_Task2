a1c9dd9cc52e1dc9b1f8b9c4b4410fb958b4f9f7
unknown <rexfordasamoah51@gmail.com>
2020-06-05 18:27:04 +0200

    update

--- scripts/Rexford.py
+++ scripts/Rexford.py
@@ -1,8 +1,2 @@
-userName = "Rexford Asamoah"
-idNum = "HNG-02432"
-email = "rexfordasamoah51@gmail.com "
-scriptType = "python"
-
-print(f"Hello World, " + "this is " + userName+ " with " + "HNGi7 ID " + idNum + " and email " + email + " using " + scriptType + " for stage 2 task")
-
+print("Hello World, this is Rexford Asamoah with HNGi7 ID HNG-02432 and email rexfordasamoah51@gmail.com using Python for stage 2 task")
 
