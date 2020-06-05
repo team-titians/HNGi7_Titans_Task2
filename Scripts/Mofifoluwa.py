cacd95b6b7e30737241eca53713f96b025fbd92c
unknown <rexfordasamoah51@gmail.com>
2020-06-05 23:21:41 +0200

    clean

---
+++ Scripts/Mofifoluwa.py
@@ -0,0 +1,2 @@
+  
+print(“Hello world, this is Adeniyi Mofifoluwa with HNG ID HNG-01280 and email adeniyiadedeji98@yahoo.com using python for task 2”)
---
+++ Scripts/kangarian-HNG-06707.js
@@ -0,0 +1,9 @@
+//task 2
+const NAME = 'Brian okanga';
+const HNG_ID = HNG-06707;
+const EMAIL = 'briannokanga@outlook.com';
+const LANGUAGE = 'JavaScript';
+
+console.log(`Hello world, my name is ${NAME}, my HNG ID is ${HNG_ID}
+             and my email is ${EMAIL} and ill be using ${LANGUAGE} 
+             for the task`);
