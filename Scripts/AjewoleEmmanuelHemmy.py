0ad33e36c7ff0e1e421c83d31c5b9b50943d022b
HemmyCoded <66225177+HemmyCoded@users.noreply.github.com>
2020-06-05 19:31:32 +0100

    latest

--- Scripts/AjewoleEmmanuelHemmy.py
+++ Scripts/AjewoleEmmanuelHemmy.py
@@ -1,1 +1,1 @@
-print("Hello World, this is Ajewole Emmanuel HNG-02822 aeoworld@gmail.com using python for stage 2 task");
+print("Hello World, this is Ajewole Emmanuel HNG-02822 aeoworld@gmail.com using python for stage 2 task")
