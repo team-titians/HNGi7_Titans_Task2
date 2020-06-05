7b7313679fdf97888d50f90931b13a8552bcc011
Olaiya Ifedapo Princewill <princewillolaiya@gmail.com>
2020-06-05 09:43:30 +0100

    Removed email

--- scripts/Olaiya_Ifedapo_Princewill-HNG-03278-Python.py
+++ scripts/Olaiya_Ifedapo_Princewill-HNG-03278-Python.py
@@ -1,9 +1,6 @@
 
 name = "Olaiya Ifedapo Princewill"
 hng_id = "HNG-03278"
-email = "princewillolaiya@gmail.com"
 language = "python"
 print(
-    "Hello World, this is " + name + " with HNGi7 ID: " + hng_id + " and email " +
-    email + " using " + language + " for stage 2 task")
-value = (name, hng_id)
+    "Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task")
