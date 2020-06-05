33598654e8eed0fe4e89e46826f2710a4b81f051
AlphaMaxRDJ <alpha.maxrdj@gmail.com>
2020-06-05 16:56:19 +0100

    Resubmissions 7 from the_Phoenix

--- scripts/the_Phoenix-HNGi7-04459.py
+++ scripts/the_Phoenix-HNGi7-04459.py
@@ -1,13 +1,6 @@
-phoenix_dict = {
-    "Name": "Precious Evwidonor",
-    "HNG_ID": "HNG-04459",
-    "Email": "peekube01@gmail.com",
-    "Language": "Python"
-}
-data1 = phoenix_dict.get("Name")
-data2 = phoenix_dict.get("HNG_ID")
-data3 = phoenix_dict.get("Email")
-data4 = phoenix_dict.get("Language")
-if "Name" and "HNG_ID" and "Email" and "Language" in phoenix_dict:
-    print("Hello World, this is ", data1, " with HNGi7 ID ", data2, " and email ", data3, " and using  ", data4, " for stage 2 task")
-#Thank you Jerry Hogan
+fullname = "Precious Evwidonor"
+HNGid  = "HNG-04459"
+email = "peekube01@gmail.com"
+lang = "Python"
+
+print('Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task'.format(fullname,HNGid, email,lang))
