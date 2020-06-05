34a8cae2a459746810d8c5ccd615b57481d305c6
Ahmad <ahmadayub918@gmail.com>
2020-06-05 11:30:28 +0300

    adding my scripts

---
+++ Scripts/Ahmad_Ayub-hng04340.js
@@ -0,0 +1,6 @@
+const name = "Ahmad";
+const hngid = "04340";
+const language = "javascript";
+const email = "ahmadayub918@gmail.com";
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + hngid + " and email " + email +" using " + language + " for stage 2 task ");
