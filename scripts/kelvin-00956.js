f012d6fabb5e3f9e45c797c1622ad5a70a88adb2
kelwaffi <64165118+kelwaffi@users.noreply.github.com>
2020-06-05 07:11:35 +0100

    Update kelvin-00956.js
    I've made corrections

--- scripts/kelvin-00956.js
+++ scripts/kelvin-00956.js
@@ -5,7 +5,7 @@ const task2= ()=>{
     const id = '00956';
     const lang= "JavaScript";
 
-    const result=`Hello World, this is ${one} with HNGi7 iD HNG-${id} and email ${email} using ${lang} for stage 2 task`;
+    const result=`Hello World, this is ${one} with HNGi7 ID HNG-${id} and email ${email} using ${lang} for stage 2 task`;
     console.log(result);
 
 }
