c6c354657d122aff46e9106f19061a8f8fbe7f6c
Busuyem <owoyemibusuyi@gmail.com>
2020-06-05 15:03:27 +0100

    Another correction

--- scripts/busuyem.php
+++ scripts/busuyem.php
@@ -1,9 +1,10 @@
 <?php
+
 $fullName = "Owoyemi Busuyi Philip";
 $email = "owoyemibusuyi0@gmail.com";
 $hng_id = "HNG-06108";
 $language = "PHP";
 
-echo "Hello World, this is $fullName with HNGi7 ID $hng_id using $language for stage 2 task.";
+echo ("Hello World, this is ". $fullName." with HNGi7 ID " .$hng_id." using ". $language." for stage 2 task");
 
 ?>
