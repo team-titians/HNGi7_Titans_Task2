9d14facd3605a87d04efbec8bbc81f5760007082
vfx07 <66240816+vfx07@users.noreply.github.com>
2020-06-05 13:51:00 +0100

    Update vfx.py

--- scripts/vfx.py
+++ scripts/vfx.py
@@ -1,1 +1,1 @@
-print ('Hello World, this is Ijeh David with HNGi7 ID [HNG-00105] using Python for stage 2 task.')
+print ('Hello World, this is Ijeh David with HNGi7 ID HNG-00105 and email dreeadreee@gmail.com using Python for stage 2 task.')
