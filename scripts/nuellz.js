148a4479b7f014c8c0db8c9cf45de1dd7ec02e28
nuella358 <64039642+nuella358@users.noreply.github.com>
2020-06-05 21:39:30 +0100

    Akachi Njoku HNG

---
+++ scripts/nuellz.js
@@ -0,0 +1,6 @@
+const name = "Akachi Njoku";
+const id = "HNG-05942";
+const language = "JavaScript";
+const email = "nuellanjoku@gmail.com";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
