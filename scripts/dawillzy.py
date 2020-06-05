06eb65544d85c9aca2a113bd11d0262a05c45b23
Wilfred <dawillzy@gmail.cm>
2020-06-05 13:32:08 +0100

    resolved some errors coursing failure

--- scripts/dawillzy.py
+++ scripts/dawillzy.py
@@ -1,5 +1,5 @@
 name = "Wilfred Omuvwie"
-id = "HNGi7-05366"
+id = "HNG-05366"
 email = "dawillzy@gmail.com"
 language = "Python"
 message = "Hello World, this is Wilfred Omuvwie with HNGi7 ID HNG-05366 using python for stage 2 task" 
