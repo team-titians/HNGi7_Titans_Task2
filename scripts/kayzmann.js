bd4a8e7a5a868acb06973375d236e7911e02c504
unknown <kayodefredrickbalogun@gmail.com>
2020-06-05 19:12:19 +0100

    feat:kayzmann.js

--- scripts/kayzmann.js
+++ scripts/kayzmann.js
@@ -1,6 +1,6 @@
 let name = "Balogun Kayode";
 let email = "kayodefredrickbalogun@gmail.com";
-let id = "HNG - 00215";
+let id = "HNG-00215";
 let lang = "Javascript";
 
 const fullDetails = `Hello World, this is ${name} with HNGi7 ID ${id} using Javascript for stage 2 task`;
