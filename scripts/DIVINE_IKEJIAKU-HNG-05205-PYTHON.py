c03b9d93dcae8ac977e82437b09662f10fef3090
Divine Ikejiaku <32369101+Jaxspan1@users.noreply.github.com>
2020-06-05 03:18:52 +0100

    Update DIVINE_IKEJIAKU-HNG-05205-PYTHON.py

--- scripts/DIVINE_IKEJIAKU-HNG-05205-PYTHON.py
+++ scripts/DIVINE_IKEJIAKU-HNG-05205-PYTHON.py
@@ -1,1 +1,14 @@
-print("Hello World, this is Ikejiaku Divine with HNGi7 ID HNG-05205 and email divineikejiaku@gmail.com using Python for stage 2 task.")
+def stage_2_task(data):
+    return_string = "Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task"
+    return(return_string.format(data['name'], data['id'], data['email'], data['language']))
+
+
+if __name__ == '__main__':
+
+    details = {"name": "Ikejiaku Divine",
+               "id": "HNG-05205",
+               "email": "divineikejiaku@gmail.com",
+               "language": "python",
+               }
+
+    print(stage_2_task(details))
