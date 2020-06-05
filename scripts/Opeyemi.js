305796db97439db706be85d951e3517724738cda
knight25 <opeyemitolulope25@gmail.com>
2020-06-05 11:47:37 +0100

    new commit

--- scripts/Opeyemi.js
+++ scripts/Opeyemi.js
@@ -1,10 +1,1 @@
-const task = () =>{
-    const fullname ="Opeyemi Tolulope";
-    const id       ="HNG-02301";
-    const email    ="opeyemitolulope25@gmail.com"
-    const language ="Javascript";
-
-    return `Hello World, this is $(fullname) with HNGi7 ID $(id) using $(language) for stage 2 task`
-}
-
-console.log(task());
+console.log("Hello World, this is Opeyemi Tolulope with HNGi7 HNG-02301 and email opeyemitolulope25@gmail.com using nodejs for stage 2 task");
