9a5bb9d136e15176cd22fbff519090c7c0a17453
alicendeh <alicendeh16@gmail.com>
2020-06-05 22:13:24 +0100

    created new script

---
+++ Scripts/aliceN.js
@@ -0,0 +1,12 @@
+let details = {
+  myName: 'Alice Ndeh',
+  id: 'HNG-00642',
+  stack: 'JavaScript',
+  email: 'alicendeh16@gmail.com',
+};
+
+const { myName, id, stack, email } = details;
+
+console.log(
+  `Hello World, this is ${myName} with HNGi7 ID ${id} and email ${email} using ${stack} for stage 2 task`
+);
