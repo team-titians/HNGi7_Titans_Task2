101c9f1691e4d8da5757dbd54c3174b28a968f1a
ahmad-boop <61063046+ahmad-boop@users.noreply.github.com>
2020-06-05 21:55:11 +0300

    Update Ahmad_Ayub-hng04340.js

--- Scripts/Ahmad_Ayub-hng04340.js
+++ Scripts/Ahmad_Ayub-hng04340.js
@@ -1,6 +1,6 @@
-const name = "Ahmad";
-const hngid = "04340";
+const fullName = "Ahmad Ayub Muhammed";
+const Id = "04340";
 const language = "javascript";
 const email = "ahmadayub918@gmail.com";
 
-console.log("Hello World, this is " + name + " with HNGi7 ID " + hngid + " and email " + email +" using " + language + " for stage 2 task ");
+console.log('Hello World, this is '+fullName+' with HNGi7 ID '+Id+' and email '+email+' using '+language+' for stage 2 task');
