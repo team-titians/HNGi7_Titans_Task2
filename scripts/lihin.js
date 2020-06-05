75fdef5f3b392028e2b199ff1683e67f0d58adcf
Lihin Ogana <53868413+Lihination@users.noreply.github.com>
2020-06-05 10:34:42 +0100

    modified  array

--- scripts/lihin.js
+++ scripts/lihin.js
@@ -1,8 +1,4 @@
-const details = [
-    ["name:","Lihin Ogana"],
-    ["id:","HNG-00423"], ["email:","lihinogana@gmail.com"],
-    ["language:", "JavaScript"] 
-]
+const details = [ "Lihin Ogana", "HNG-00423", "lihinogana@gmail.com", "javascript"]
 
     console.log("Hello World, this is " + details[0] +" with HNGi7 ID " + details[1] + " and email " + details[2] + " using " + details[3] +" for stage 2 task")
 
