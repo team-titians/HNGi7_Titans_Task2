8bce50b62bdace538d263a1443c933734dfaa272
Wilfred <dawillzy@gmail.cm>
2020-06-05 10:50:19 +0100

    edited the message output

--- scripts/dawillzy.py
+++ scripts/dawillzy.py
@@ -2,5 +2,5 @@ name = "Wilfred Omuvwie"
 id = "HNGi7-05366"
 email = "dawillzy@gmail.com"
 language = "Python"
-message = "Hello World, this is Wilfred Omuvwie with ID HNGi7-05366 using python for stage 2 task" 
+message = "Hello World, this is Wilfred Omuvwie with HNGi7 ID HNG-05366 using python for stage 2 task" 
 print(message)
