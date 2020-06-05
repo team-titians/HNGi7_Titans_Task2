7a2be7ad65438a029f87bcf8fb4db01909cf3185
t-bolt <isholasobuur@gmail.com>
2020-06-05 02:11:37 +0100

    First commit of task

---
+++ scripts/Sobuur.php
@@ -0,0 +1,8 @@
+<?php
+	$myName = "Ishola Abdul-Sobuur";
+	$myId = "HNG-02985";
+	$myEmail = "crospoghosti@gmail.com";
+	$myFullDetails = "Hello World, this is ".$myName." with HNGi7 ID ".$myId." and email ".$myEmail." using php for stage 2 task";
+	echo $myFullDetails;
+
+?>
