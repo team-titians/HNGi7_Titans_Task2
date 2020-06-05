abdcc523a8fac67ba9b4df8965a6e6a5a67582f3
Wilfred Omuvwie <40261136+Dawillzy@users.noreply.github.com>
2020-06-05 16:50:20 +0100

    Removed some white space

--- scripts/dawillzy.js
+++ scripts/dawillzy.js
@@ -3,4 +3,4 @@ var id = "HNG-05366";
 var email = "dawillzy@gmail.com";
 var language = "Javascript";
 
-console.log("Hello World, this is " + name + "with HNGi7 ID " + id + " using " + language + " for stage 2 task");
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task");
