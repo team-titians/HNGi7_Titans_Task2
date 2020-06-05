0ad48ec9e7fb778d8bc7d232584301b3aba157d0
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-05 23:40:02 +0200

    Delete Ahmad_Ayub-hngid04340.js

--- Scripts/Ahmad_Ayub-hngid04340.js
+++
@@ -1,5 +0,0 @@
-var fullName = 'Ahmad Ayub Mohammed';
-var Id = 'HNG-04340';
-var language = 'JavaScript';
-var email = 'ahmadayub918@gmail.com';
-console.log('Hello World, this is '+fullName+' with HNGi7 ID '+Id+' and email '+email+' using '+language+' for stage 2 task');
