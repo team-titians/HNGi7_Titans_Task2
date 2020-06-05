f97b679a2b3f5e4eddb79d3e46db7cef94d7da8a
ahmad-boop <61063046+ahmad-boop@users.noreply.github.com>
2020-06-05 11:24:43 +0300

    Delete Ahmad_Ayub-hng04340.js

--- Ahmad_Ayub-hng04340.js
+++
@@ -1,6 +0,0 @@
-const name = "Ahmad";
-const hngid = "04340";
-const language = "javascript";
-const email = "ahmadayub544@gmail.com";
-
-console.log("Hello World, this is " + name + " with HNGi7 ID " + hngid + " and email " + email +" using " + language + " for stage 2 task ");
