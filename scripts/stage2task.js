ddd5b15f9e9a742864c3ca967d58f73a8fbd865f
bauxitex3 <66308671+bauxitex3@users.noreply.github.com>
2020-06-05 07:01:47 -0700

    deleted double scripts

--- scripts/stage2task.js
+++
@@ -1,8 +0,0 @@
-stage2task =()=>{
-const name="Malcom Bello";
-const id= "HNG-02832";
-const email="bauite.x3@gmail.com";
-lang= "Node.js";
-console.log("hello world, this is ", name, 'with HNGi7 ID ',id, 'with email ', email, "using ", lang, "for stage 2 task");
-}
-stage2task()
