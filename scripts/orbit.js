f03c2b2a9f58828b6052c2ffe72c3c2a13410d0f
abdul-o <abdullateefdauda01@gmail.com>
2020-06-05 09:24:48 +0100

    correction of world to World

--- scripts/orbit.js
+++ scripts/orbit.js
@@ -3,6 +3,6 @@ const id = "HNG-01775";
 const email = "abdullateefdauda01@gmail.com"
 const language = "javaScript";
 
-let profileDisplay = "Hello world, this is " + fullName + " with HNGi7 ID " + id +" and email " + email + " using " + language + " for stage 2 task";
+let profileDisplay = "Hello World, this is " + fullName + " with HNGi7 ID " + id +" and email " + email + " using " + language + " for stage 2 task";
 
 console.log(profileDisplay);
