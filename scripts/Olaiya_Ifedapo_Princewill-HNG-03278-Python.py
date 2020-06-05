0768f04454fbec4fa16be9e706f4329000b6b2e1
Olaiya Ifedapo Princewill <princewillolaiya@gmail.com>
2020-06-05 09:30:34 +0100

    corrected

--- scripts/Olaiya_Ifedapo_Princewill-HNG-03278-Python.py
+++ scripts/Olaiya_Ifedapo_Princewill-HNG-03278-Python.py
@@ -4,6 +4,6 @@ hng_id = "HNG-03278"
 email = "princewillolaiya@gmail.com"
 language = "python"
 print(
-    "Hello World, this is " + name + "with HNGi7 ID: " + hng_id + "and email " +
-    email + " using" + language + " for stage 2 task")
+    "Hello World, this is " + name + " with HNGi7 ID: " + hng_id + " and email " +
+    email + " using " + language + " for stage 2 task")
 value = (name, hng_id)
