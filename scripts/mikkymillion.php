6787a419f799454485907cf40a854fe4c483d103
Lewis <ofili@users.noreply.github.com>
2020-06-05 21:15:02 +0100

    commit

---
+++ scripts/mikkymillion.php
@@ -0,0 +1,6 @@
+<?php
+$NAME = "Adesanya Temitope";
+$ID = "HNG-02729";
+$language = 'PHP';
+$email = "atmonemillion@gmail.com";
+echo( "Hello World, this is $NAME with HNGi7 ID $ID and email $email using $language for stage 2 task");
