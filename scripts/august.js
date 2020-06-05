47d0ececc3d0ed35cbf7475a963d856e6ea6f35f
AugustBemsen <54921134+AugustBemsen@users.noreply.github.com>
2020-06-05 10:43:49 +0100

    Update august.js
    Fixed mistake

--- scripts/august.js
+++ scripts/august.js
@@ -1,6 +1,5 @@
 const name = 'Daniel Bemsen';
 const myId = 'HGN-06647';
-const email = 'danireptor7@gmail.com';
-const language = 'JavaScript';
+const language = 'javaScript';
 
-console.log(`Hello world my name is ${name} with HNGi7 ID ${myId} and email ${email}, using ${language} for stage 2 task `);
+console.log(`Hello World, my name is ${name} with HNGi7 ID ${myId} using ${language} for stage 2 task`);
