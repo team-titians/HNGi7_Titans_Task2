86b348278d08acc5546d8b98b26eac28ffb59040
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-05 23:40:46 +0200

    Delete AjewoleEmmanuelHemmy.js

--- Scripts/AjewoleEmmanuelHemmy.js
+++
@@ -1,6 +0,0 @@
-const name = "Ajewole Emmanuel";
-const hngId = "HNG-02822";
-const language = "Javascript";
-const email = "aeoworld@gmail.com";
-
-console.log(`Hello World, this is ${name} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`);
