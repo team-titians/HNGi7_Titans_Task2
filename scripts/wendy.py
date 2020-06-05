53437ec369a22bec22e68886912e106b44809b71
wendymunyasi <wendymunyasi@gmail.com>
2020-06-05 11:30:54 +0300

    Remove email from wendy.py.

--- scripts/wendy.py
+++ scripts/wendy.py
@@ -7,7 +7,6 @@ my_details = '''
     {
         "name": "Wendy Munyasi",
         "id": "HNG-02150",
-        "email": "wendymunyasi@gmail.com",
         "language": "Python3"
     }
 
@@ -15,4 +14,4 @@ my_details = '''
 
 data = json.loads(my_details)
 
-print("Hello World, this is " + data['name']  +  " with HNGi7 ID " + data['id'] + "and email " + data['email'] + " using " + data['language'] + " for stage 2 task")
+print("Hello World, this is " + data['name']  +  " with HNGi7 ID " + data['id'] + " using " + data['language'] + " for stage 2 task")
