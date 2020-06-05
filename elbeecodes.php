2d25f7ad640849fba419876768e28d16b30f6c17
omololabalogun <57988544+omololabalogun@users.noreply.github.com>
2020-06-05 06:33:13 +0100

    Add files via upload

---
+++ elbeecodes.php
@@ -0,0 +1,12 @@
+<?php
+
+//echo "Hello World, this is Omolola Balogun with HNGi7 ID: HNG-02016, and elbeecodes@gmail.com using php for stage 2 task.";
+$fullname ="Omolola Balogun";
+$hngi7id = "HNG-02016";
+$email = "elbeecodes@gmail.com";
+$language = "PHP";
+
+echo "Hello World, this is ${fullname} with HNGi7 ID ${hngi7id} and email ${email} using ${language} for stage 2 task";
+
+
+?>
