0fe8d40a420245febd00045d929c2707c0d0b4f2
Wilfred Omuvwie <40261136+Dawillzy@users.noreply.github.com>
2020-06-05 21:26:21 +0100

    updated dawillzy.py

--- scripts/dawillzy.py
+++ scripts/dawillzy.py
@@ -1,8 +1,8 @@
-mName = "Wilfred Omuvwie"
-ID ="HNG-05366"
-myEmail = "dawillzy@gmail.com"
+name = "Wilfred Omuvwie"
+id ="HNG-05366"
+email = "dawillzy@gmail.com"
 language = "Python"
 
-message = f"Hello World, this is {myName} with HNGi7 ID {ID} and email {myEmail} using {language} for stage 2 task"
+message = f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
 
 print(message)
