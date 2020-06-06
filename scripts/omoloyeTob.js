1c88df5eb37c2c37f20aaea69b95e1af666e456d
Omoloye Oluwatobi <64864148+omoloye@users.noreply.github.com>
2020-06-06 17:37:47 +0100

    Edited file

---
+++ scripts/omoloyeTob.js
@@ -0,0 +1,12 @@
+function hello () {
+    
+    var name = "Omoloye Oluwatobi ";
+    var id = "HNG-03114";
+    var email = "tobi_omoloye@yahoo.com";
+    var language = "javascript";
+    return (
+        `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+    )
+};
+
+console.log(hello());
