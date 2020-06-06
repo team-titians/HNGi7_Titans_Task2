f0e42c64512353210b9a2a0d0965d4e37ef0957a
kdavis2020 <66250513+kdavis2020@users.noreply.github.com>
2020-06-06 11:00:45 +0100

    Update kdavis.php

--- scripts/kdavis.php
+++ scripts/kdavis.php
@@ -1,5 +1,5 @@
 <?php
 
-echo("Hello World, this is David Bello with HNGi7 ID HNG-02527 using PHP for stage 2 task");
+echo("Hello World, this is David Bello with HNGi7 ID HNG-02527 and email davidbello.a@gmail.com using PHP for stage 2 task");
 
 ?>
