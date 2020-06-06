d117264e675135fea89b68e6dcbe455e80c6140a
heywhygirl <40364908+heywhygirl@users.noreply.github.com>
2020-06-06 22:00:31 +0100

    Create est33y.py

---
+++ scripts/est33y.py
@@ -0,0 +1,6 @@
+userName = "Ayobami Olanrewaju"
+idNum = "HNG-01902"
+email = "esteey22@gmail.com "
+scriptType = "python"
+
+print(f"Hello World, " + "this is " + userName+ " with " + "HNGi7 ID " + idNum + " and email " + email + " using " + scriptType + " for stage 2 task")
