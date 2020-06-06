5b67c2a64cf8f8a905d04e523071dd6f9ea7bf8d
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-07 00:17:44 +0200

    Update est33y.js

--- scripts/est33y.js
+++ scripts/est33y.js
@@ -2,5 +2,5 @@ let name = "Ayobami Olanrewaju";
 let hngID ="HNG-01902";
 let email ="esteey22@gmail.com";
 let language="Javascript";
-let output = "Hello World, this is "+name+" with HNGi7 ID" +hngID+ "and email" +email+ "using" +language+ "for stage 2 task";
+let output = " Hello World, this is " + name + " with HNGi7 ID " + hngID + " and email " + email + " using " + language + " for stage 2 task ";
 console.log(output);
