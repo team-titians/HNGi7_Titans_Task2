5657d409c57bf84677557ba5fa9a58aec3652082
oladolapo <66223952+oladolapo@users.noreply.github.com>
2020-06-06 10:21:43 +0100

    Update ollie.py

--- scripts/ollie.py
+++ scripts/ollie.py
@@ -1,7 +1,8 @@
-def introduction(name, ID, language):
-    result = "Hello world, this is {} with HNGi7 ID  {}  using {} for stage 2 task"
-    return (result.format(name, ID, language))
+def introduction(name, ID, Email, language):
+    result = "Hello world, this is {} with HNGi7 ID  {} and email {} using {} for stage 2 task"
+    return (result.format(name, ID, Email, language))
 
 
-message = introduction("Ogunsipe Oladolapo", "HNG-00590", "python")
+message = introduction("Ogunsipe Oladolapo", "HNG-00590",
+                       "dolapoogunsipe@gmail.com", "python")
 print(message)
