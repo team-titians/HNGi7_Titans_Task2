9f2a6c93a19c8b4dd743fe7f3645b8c7edc8cf62
EmemOkon <ememokon3@gmail.com>
2020-06-06 11:19:08 +0100

    add ememokon script

--- Scripts/EmemOkon.py
+++ Scripts/EmemOkon.py
@@ -3,7 +3,7 @@ def okon():
 	name = "Emem Okon"
 	Hngid = "HNG-06413"
 	email = "ememokon3@gmail.com"
-	return f"Hello World this is {name} with HNGi7 ID {Hngid} and email address {email} using python3 for stage 2 task"
+	return f"Hello World this is {name} with HNGi7 ID {Hngid} and email {email} using python3 for stage 2 task"
 	
 if __name__ == "__main__":
 	print(okon())
