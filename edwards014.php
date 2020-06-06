74725352c7bf1f1d5eaffc08d19737dc327a1add
ojayedwards <ojayedwards1@yahoo.com>
2020-06-06 02:31:15 +0100

    updated details

---
+++ edwards014.php
@@ -0,0 +1,10 @@
+<?php
+$fullname = "Edward Ajayi";
+$id = "HNG-06171";
+$email = "ojayedwards01@gmail.com";
+$language = "php";
+echo("Hello World, this is " . 
+$fullnamename . " with HNGi7 ID " . 
+$id . " and email " . $email . " using " 
+. $language . " for stage 2 task");
+ ?>
