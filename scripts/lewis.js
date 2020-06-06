146e560c689215dadd5500484a28a9e703d6091a
Lewis <ofili@users.noreply.github.com>
2020-06-06 08:40:46 +0100

    commit

--- scripts/lewis.js
+++ scripts/lewis.js
@@ -1,12 +1,1 @@
-var person = {
-    fullname: "Ofili Lewis",
-    hng_id: "HNG-00427",
-    email: "ofililewis@gmail.com",
-    language: "Golang",
-}
-
-const say = () => {
-    var output = `Hello world, this is ${person.fullname} with HNGi7 ID ${person.hng_id} and email ${person.email} using ${person.language} for stage 2 task`;
-    return console.log(output);
-}
-say();
+console.log("Hello World, this is Ofili Lewis with HNGi7 ID HNG-00427 using Golang for stage 2 task");
