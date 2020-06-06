2d1e83633775a2693820955acacd6493aa66a8df
Oluwatosin017 <66261149+Oluwatosin017@users.noreply.github.com>
2020-06-06 15:35:04 +0100

    Chore

--- scripts/Oluwatosin.py
+++ scripts/Oluwatosin.py
@@ -1,7 +1,7 @@
 fullname = "Oluwatosin Taiwo"
-      id = "HNG-01224"   
-   email = "ttvicktor1@gmail.com "   
+id = "HNG-01224"   
+email = "ttvicktor1@gmail.com"   
 language = "python"
 
 
-print("Hello World, this is" + fullname + "with HNGi7 ID" + id + "and email" + email + "using" + language + "for stage 2 task").
+print("Hello World, this is " + fullname + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task.")
