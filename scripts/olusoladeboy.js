6e8016df3b26b30651a67c0702bb930f9070ca22
Emmanuel Olusola <eoyewole508@stu.ui.edu.ng>
2020-06-06 23:28:33 +0100

    Update olusoladeboy.js

--- scripts/olusoladeboy.js
+++ scripts/olusoladeboy.js
@@ -1,6 +1,8 @@
-var name="Emmannuel Olusola Oyewole";
-var id= "HNG-05201";
-var email="eoyewole508@stu.ui.edu.ng";
-var language= "Javascript";
-const output = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
-console.log(output);
+var name = 'Emmanuel Oyewole';
+var id = 'HNG-05201';
+var language = 'Javascript';
+var email = 'eoyewole508@stu.ui.edu.ng';
+const hngTask=()=>{
+	return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
+}
+console.log(hngTask());
