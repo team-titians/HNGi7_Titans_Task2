040f969b22d572f527aadef26f3456dcbbfb3b57
oladolapo <66223952+oladolapo@users.noreply.github.com>
2020-06-06 09:47:11 +0100

    Delete ollie.py

--- scripts/ollie.py
+++
@@ -1,8 +0,0 @@
-def introduction(name, ID, Email, language):
-    result = "Hello world, this is {} with HNGi7 ID  {} and email {} using {} for stage 2 task"
-    return (result.format(name, ID, Email, language))
-
-
-message = introduction("Ogunsipe Oladolapo", "HNG-00590",
-                       "dolapoogunsipe@gmail.com", "python")
-print(message)
