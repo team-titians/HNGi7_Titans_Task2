d263a69e38a39c55e524bba5aa5ee5ecd640a346
Rufai Kudus Boye <rufaikudus2014@gmail.com>
2020-06-06 23:15:48 +0100

    Rufai stage 2 task

---
+++ scripts/Rufaikudus.js
@@ -0,0 +1,11 @@
+let name,id,email,language;
+
+name = "Rufai Kudus Adeboye";
+id   = "HNG-02684";
+email = "rufaikudus2014@gmail.com";
+langauge = "Javascript";
+
+function details(name,id,email,langauge){
+    return 'Hello World, this is '+name+'  with HNGi7 ID '+id+' and email '+email+' using '+langauge+' for stage 2 task';
+}
+console.log(details(name,id,email,langauge));
