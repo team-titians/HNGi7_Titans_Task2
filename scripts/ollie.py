17f97239022333a14c5a4a757560024544f6e6ee
oladolapo <66223952+oladolapo@users.noreply.github.com>
2020-06-06 09:48:06 +0100

    Add files via upload

---
+++ scripts/ollie.py
@@ -0,0 +1,7 @@
+def introduction(name, ID, language):
+    result = "Hello world, this is {} with HNGi7 ID  {}  using {} for stage 2 task"
+    return (result.format(name, ID, language))
+
+
+message = introduction("Ogunsipe Oladolapo", "HNG-00590", "python")
+print(message)
