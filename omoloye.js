48c18a575e2bcaddfb89a58e87ee1f67d5043be6
Omoloye Oluwatobi <64864148+omoloye@users.noreply.github.com>
2020-06-05 21:08:40 -0400

    Create omoloye.js

---
+++ omoloye.js
@@ -0,0 +1,12 @@
+function omoloye () {
+    
+    var name = "Omoloye oluwatobi";
+    var id = "HNG-003114";
+    var email = "tobi_omoloye@yahoo.com";
+    var language = "javascript";
+    return (
+        `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+    )
+}
+
+console.log(omoloye());
