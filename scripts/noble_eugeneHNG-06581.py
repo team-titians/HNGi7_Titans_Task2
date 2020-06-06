9cc523c12db4100c26686d83778f7b68611fc59e
nobleeugene <54331437+nobleeugene@users.noreply.github.com>
2020-06-06 22:13:47 +0100

    nobleeugene python script commited

---
+++ scripts/noble_eugeneHNG-06581.py
@@ -0,0 +1,2 @@
+inputs= ["ABDULWASIU IBRAHIM","HNG-06581","abdulwasiuibrahim2000@gmail.com","Python"]
+print("Hello World, this is",inputs[0],"with HNGi7 ID",inputs[1],"and email",inputs[2],"using",inputs[3],"for stage 2 task")
