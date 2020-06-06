71d75697f76c6e2e7c46a07cb80c0fb3bbddcc8f
YEMIOYE <42058605+YEMIOYE@users.noreply.github.com>
2020-06-07 00:37:08 +0100

    Update YemiOye.js

--- scripts/YemiOye.js
+++ scripts/YemiOye.js
@@ -1,7 +1,7 @@
-//Promotion to Stage 2. All about me!😃
+
 var name = "Opeyemi Oyeyipo";
-var hngID = "HNGi7 ID_02782";
+var hngID = "HNGi7 ID HNG-02782";
 var email = "opeyemioyeyipo7@gmail.com";
 var languageUsed = "Javascript";
 
-console.log("Hello World, this is " + name + " with " + hngID + " with my email " + email + " using " + languageUsed + " for stage 2 task. ");
+console.log("Hello World, this is " + name + " with " + hngID + " and email " + email + " using " + languageUsed + " for stage 2 task. ");
