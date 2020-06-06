55fc80c7b6c1b52706502e5286cf016ebe73d52f
Haruna <66220668+HarunaOye@users.noreply.github.com>
2020-06-06 21:44:46 +0100

    No words

--- scripts/haruna.js
+++ scripts/haruna.js
@@ -3,4 +3,4 @@ let id = "HNG-01659";
 let email = "harunaibrahimoye@gmail.com";
 let language= "javascript";
 
-console.log('Hello World, this is '+ name +' with HNGi7 ID '+ id +' and email '+ email +' using '+ language +'for stage 2 task');
+console.log('Hello World, this is '+ name +' with HNGi7 ID '+ id +' and email '+ email +' using '+ language +' for stage 2 task');
