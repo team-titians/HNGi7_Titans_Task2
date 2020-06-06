ccda3c1efae5ee0cd3edc285f621c6908d66668b
engremerald <58258512+engremerald@users.noreply.github.com>
2020-06-06 08:38:25 +0100

    emerald scrpit

---
+++ scripts/emerald.js
@@ -0,0 +1,5 @@
+var fullName="Aitomun Faith";
+var HNGi7_ID="HNG-02783";
+var LANGUAGE_USED="javascript";
+var email="faithaitomun@gmail.com";
+console.log('Hello World,' +'this is'+ ' '+ fullName +' '+ 'with HNGi7 ID'+' '+ HNGi7_ID +' '+'and email '+' '+email+' '+   'using'+' '+ LANGUAGE_USED+' '+ 'for stage 2 task');
