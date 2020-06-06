ac89f1f635f8b09d4e12c0c32c22a347cb8b263c
ABU HUZAIFA <huzaius@gmail.com>
2020-06-06 12:51:39 +0000

    removed dashes at eof

--- scripts/huzaius.py
+++ scripts/huzaius.py
@@ -10,4 +10,4 @@ fullname = 'Abu Huzaifa'
 internID = 'HNG-02607'
 email = 'habu@st.ug.edu.gh'
 language = 'python'
-print('_Hello World, this is',fullname,'with HNGi7 ID',internID,'and email',email,'using',language,'for stage 2 task_ .')
+print('Hello World, this is',fullname,'with HNGi7 ID',internID,'and email',email,'using',language,'for stage 2 task.')
