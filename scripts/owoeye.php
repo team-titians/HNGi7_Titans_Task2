a82446dbf8c75be6d5799a6a346a7082c9090906
Owoeye1321 <owoeye1321@gmail.com>
2020-06-06 08:17:09 +0100

    Create owoeye.php

---
+++ scripts/owoeye.php
@@ -0,0 +1,12 @@
+<?php
+            function greet_hng($name,$hng_id,$language,$email){
+                
+                $output = "Hello World,this is ".$name." with HNGi7 ID HNG".$hng_id." using "
+                .$language." for stage 2 ".$email ;
+                return $output;
+            }
+             
+             echo  greet_hng("OWOEYE SAMUEL",02249,"PHP",'Owoeye1321@gmail.com');
+
+
+?>
