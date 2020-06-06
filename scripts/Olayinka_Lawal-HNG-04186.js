5d1948256be5db398c7ec83dc5081253880afadd
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-06-06 23:56:36 +0100

    Olayinka HNG-04186

--- scripts/Olayinka_Lawal-HNG-04186.js
+++ scripts/Olayinka_Lawal-HNG-04186.js
@@ -1,7 +1,7 @@
 let myName = {
-    name: "Lawal Olayinka Mudashiru",
-    id:   "HNG-04186",
-    language: "javaScript",
+    name:"Lawal Olayinka Mudashiru",
+    id:"HNG-04186",
+    language:"javaScript",
 }
 let result = "Hello world, this is " + myName.name + " with HNGi7 ID " + myName.id + " using " + myName.language + " for stage 2 task"
 
