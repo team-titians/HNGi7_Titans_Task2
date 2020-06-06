a67dfe23ba65db97f1ca8bb68eb9ca18703e3e31
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-06 11:18:25 +0200

    Create emerald.js

---
+++ scripts/emerald.js
@@ -0,0 +1,6 @@
+var name = "Aitomun Faith"
+var language_Used = "Javascript"
+var Email = "faithaitomun@gmail.com"
+var hng_Id = "HNG-02783"
+var result = "Hello World, this is "+ name +" with HNGi7 ID "+ hng_Id + " and email "+ Email+" using "+ language_Used + " for stage 2 task"
+console.log(result);
