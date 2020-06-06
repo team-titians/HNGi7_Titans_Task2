c0ed198bf3cdd02fcd71c13c48e3614f3f790d8a
Joseph Godwin <55412694+Obutehj@users.noreply.github.com>
2020-06-06 21:05:15 +0100

    Joebrown.php file added via file upload

---
+++ Joebrown.php
@@ -0,0 +1,10 @@
+<?php
+
+$fullname = "Joseph Godwin";
+$hng_id = "HNG-06789";
+$language = "PHP";
+$email = "jojoebrown129@gmail.com";
+
+echo "Hello World, this is $fullname with HNGi7 ID $hng_id and email $email using $language for stage 2 task";
+
+?>
