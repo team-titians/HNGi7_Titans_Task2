a17e5a1f900aab2d328454d8e47cb46d41fef074
YEMIOYE <42058605+YEMIOYE@users.noreply.github.com>
2020-06-06 23:57:32 +0100

    Update YemiOye.js

--- scripts/YemiOye.js
+++ scripts/YemiOye.js
@@ -1,7 +1,7 @@
-
 var name = "Opeyemi Oyeyipo";
 var hngID = "HNGi7 ID_02782";
 var email = "opeyemioyeyipo7@gmail.com";
 var languageUsed = "Javascript";
 
 console.log("Hello World, this is " + name + " with " + hngID + " with my email " + email + " using " + languageUsed + " for stage 2 task. ");
+
