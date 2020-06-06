f9ce7be8ddf27b4fe97c3352b0fd05d3bf3a7f33
Oluwatosin017 <66261149+Oluwatosin017@users.noreply.github.com>
2020-06-06 11:30:59 +0100

    Create Oluwatosin.py

---
+++ scripts/Oluwatosin.py
@@ -0,0 +1,7 @@
+fullname = "Oluwatosin Taiwo"
+      id = "HNG-01224"   
+   email = "ttvicktor1@gmail.com "   
+language = "python"
+
+
+print("Hello World, this is" + fullname + "with HNGi7 ID" + Id + "and email" + email + "using" + language + "for stage 2 task").
