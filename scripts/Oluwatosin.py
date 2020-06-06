6088a349400bdbda348fe28b8365b9195fd8f16c
Oluwatosin017 <66261149+Oluwatosin017@users.noreply.github.com>
2020-06-06 03:56:54 +0100

    Create Oluwatosin.py

---
+++ scripts/Oluwatosin.py
@@ -0,0 +1,7 @@
+userName = "Oluwatosin Taiwo"
+   idNum = "HNG-02214"   
+email = "ttvicktor1@gmail.com "   
+scriptType = "python"       
+
+
+print(f"Hello World, " + "this is " + userName+ " with " + "HNGi7 ID " + idNum + " and email " + email + " using " + scriptType + " for stage 2 task")
