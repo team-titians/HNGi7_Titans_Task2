b0fcf4c159bf2291e8eb81047c84a86011af8ff3
babadee24 <66337588+babadee24@users.noreply.github.com>
2020-06-06 09:37:18 +0100

    Update Oladayo.js

--- scripts/Oladayo.js
+++ scripts/Oladayo.js
@@ -5,4 +5,4 @@ const myData = {
     hngld:"HNG-00210"
 };
 
-console.log(`Hello World, this is ${myData.name} with HNGi7 ID ${myData.hngld } and email ${myData.email} using ${myData.language} for stage 2 task`)
+console.log(`Hello World, this is ${myData.name} with HNGi7 ID ${myData.hngld} and email ${myData.email} using ${myData.language} for stage 2 task`)
