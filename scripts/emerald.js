150e9b502832495f325708ff80654987bb763989
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-06 10:22:06 +0200

    Update emerald.js

--- scripts/emerald.js
+++ scripts/emerald.js
@@ -2,4 +2,4 @@ var fullName="Aitomun Faith";
 var HNGi7_ID="HNG-02783";
 var LANGUAGE_USED="javascript";
 var email="faithaitomun@gmail.com";
-console.log('Hello World,' +'this is'+ ' '+ fullName +' '+ 'with HNGi7 ID'+' '+ HNGi7_ID +' '+'and email '+' '+email+' '+   'using'+' '+ LANGUAGE_USED+' '+ 'for stage 2 task');
+console.log('Hello World,' +'this is'+ fullName + 'with HNGi7 ID' + HNGi7_ID + 'and email '+ email  +  'using' + LANGUAGE_USED + 'for stage 2 task');
