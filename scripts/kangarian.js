8f906c1476abca6275744aa2b07e0f1fb1303fb4
brianokanga <briannokanga@gmail.com>
2020-06-06 14:17:24 +0300

    updated js file for task 2

--- scripts/kangarian.js
+++ scripts/kangarian.js
@@ -1,8 +1,11 @@
 //task 2
-const name = 'Brian Okanga';
-const id = 'HNG-06707';
-const email = 'briannokanga@outlook.com';
-const language = 'JavaScript';
+let name = 'Brian Okanga-Kangarian';    //Name declaration
+
+let id = 'HNG-06707';   //HNG ID declaration
+
+let email = 'briannokanga@outlook.com';   //Email declaration
+
+let language = 'JavaScript';    //Language declaration
 
 
-console.log(`Hello world, This is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
+console.log(`Hello world, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
