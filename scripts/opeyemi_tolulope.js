a48cee43bb8af91411953af9d0ffa38dbc9f222d
knight25 <opeyemitolulope25@gmail.com>
2020-06-06 16:47:31 +0100

    new commit

--- scripts/opeyemi_tolulope.js
+++ scripts/opeyemi_tolulope.js
@@ -2,4 +2,4 @@ var fullName = "Opeyemi Tolulope";
 var email ="opeyemitolulope25@gmail.com";
 var id ="HNG-02301";
 var language ="javascript";
-console.log("Hello World, this is" + fullName + "with HNGi7 ID" + id + "and email" + email + "using" + language + "for stage 2 task" );
+console.log("  Hello World, this is  " + fullName + "  with HNGi7 ID  " + id + "  and email  " + email + "  using  " + language + "  for stage 2 task   " );
