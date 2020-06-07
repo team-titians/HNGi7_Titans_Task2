08db209569351f5e1956ecd3e92511f1ad77f50e
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-07 16:35:57 +0200

    Delete Nonye.php

--- Nonye.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$fullName = "Agu chinonye sixtus";
-$emailAddress = "chinonye_agu@yahoo.com";
-$hng_ID = "HNG-00804";
-$language = "PHP";
-
-echo "Hello world, this is $fullName wuth HNGi7 ID $hng_ID and email $emailAddress using $language for stage 2 task";
-?>
