7618518800d6662b30fd7360577c20c7f8ae4df1
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-06-07 10:03:16 +0100

    Olayinka Lawal HNG-04186

--- scripts/Olayinka_Lawal-HNG-04186.js
+++ scripts/Olayinka_Lawal-HNG-04186.js
@@ -1,8 +1,12 @@
+
 let myName = {
-    name:"Lawal Olayinka Mudashiru",
-    id:"HNG-04186",
-    language:"javaScript",
+    name: "Lawal Olayinka Mudashiru",
+    id:   "HNG-04186",
+    email: "mudamoon97@gmail.com",
+    language: "javaScript",
+
 }
-let result = "Hello World, this is " + myName.name + " with HNGi7 ID " + myName.id + " using " + myName.language + " for stage 2 task"
+
+let result = "Hello World, this is " + myName.name + " with HNGi7 ID " + myName.id + " and email " + myName.email + " using " + myName.language + " for stage 2 task"
 
 console.log(result); 
