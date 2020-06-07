b0d7b715fdf467f3e2a825683c4cdcfcc0bdb8ee
Rexford Asamoah <36260221+Rexfordasamoah51@users.noreply.github.com>
2020-06-07 02:50:29 +0200

    Update olusoladeboy.js

--- scripts/olusoladeboy.js
+++ scripts/olusoladeboy.js
@@ -1,6 +1,6 @@
 var name = " Emmanuel Oyewole ";
 var id = " HNG-05201 ";
-var email = " eoyewole508@stu.ui.edu.ng ";
+var email = " eoyewole508@gmail.com ";
 var language = " Javascript ";
 const hngTask=()=>{
 	return "Hello World, this is" + name + "with HNGi7 ID" + id + "and email" + email + "using" + language + "for stage 2 task";
