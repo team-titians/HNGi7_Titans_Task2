70ad3e27155e807516e396498e2ebb684bb5898d
nehkee <46467065+nehkee@users.noreply.github.com>
2020-06-07 13:20:05 +0100

    Update cocopops.js
    i have resolved the syntax error

--- scripts/cocopops.js
+++ scripts/cocopops.js
@@ -5,5 +5,5 @@ let HNGid = "HNG-00066";
 let language = "Javascript";
 let email = "chukwunekeprince@gmail.com";
 
-console.log("Hello world, this is "+name+" with HNGi7 ID "+HNGid+" using "+language+" for stage 2 task "+" and "+email"");
+console.log("Hello world, this is "+name+" with HNGi7 ID "+HNGid+" and email "+email+" using "+language+" for stage 2 task");
 //modified
