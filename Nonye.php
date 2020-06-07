5187f284f91037ce077a9992a44d68cf7e76c956
kelvinsixtu <66322645+kelvinsixtu@users.noreply.github.com>
2020-06-07 04:51:21 -0700

    update nonye.php

---
+++ Nonye.php
@@ -0,0 +1,9 @@
+<?php
+
+$fullName = "Agu chinonye sixtus";
+$emailAddress = "chinonye_agu@yahoo.com";
+$hng_ID = "HNG-00804";
+$language = "PHP";
+
+echo "Hello world, this is $fullName wuth HNGi7 ID $hng_ID and email $emailAddress using $language for stage 2 task";
+?>
