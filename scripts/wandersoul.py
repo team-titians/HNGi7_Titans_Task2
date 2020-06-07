95e42577f52a6ff3b88d13eef1ca9ee4159e4550
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2020-06-07 19:43:31 +0530

    Create wandersoul.py

---
+++ scripts/wandersoul.py
@@ -0,0 +1,8 @@
+# variables containing my submission data
+full_name = "Chandra Sekhar Mullu"
+internship_id = "HNG-03304"
+language_used = "Python"
+email = "sekharmullu@gmail.com"
+
+# print the in the required format to stdout
+print("Hello World, this is "+ full_name + " with HNGi7 ID " + internship_id + " using " + language_used + " for stage 2 task. " + email)
