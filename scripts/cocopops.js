d528ac00cd13fd11b7464780f93ae8077253771f
nehkee <46467065+nehkee@users.noreply.github.com>
2020-06-07 13:30:34 +0100

    Update cocopops.js
    corrected . please review and evaluate. thanks.

--- scripts/cocopops.js
+++ scripts/cocopops.js
@@ -3,7 +3,7 @@
 let name = "Chukwuneke Prince";
 let HNGid = "HNG-00066";
 let language = "Javascript";
-let email = "chukwunekeprince@gmail.com";
+let email = "Chukwunekeprince@gmail.com";
 
 console.log("Hello world, this is "+name+" with HNGi7 ID "+HNGid+" and email "+email+" using "+language+" for stage 2 task");
 //modified
