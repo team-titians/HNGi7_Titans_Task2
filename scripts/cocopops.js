6366c0ea991696d1ecce68353e1cc69cac132d1e
nehkee <46467065+nehkee@users.noreply.github.com>
2020-06-08 01:08:37 +0100

    Delete cocopops.js

--- scripts/cocopops.js
+++
@@ -1,9 +0,0 @@
-
-
-let name = "Chukwuneke Prince";
-let HNGid = "HNG-00066";
-let language = "Javascript";
-let email = "Chukwunekeprince@gmail.com";
-
-console.log("Hello world, this is "+name+" with HNGi7 ID "+HNGid+" and email "+email+" using "+language+" for stage 2 task");
-//modified
